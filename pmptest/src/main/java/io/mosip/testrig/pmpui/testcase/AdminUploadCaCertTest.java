1af5f0b4d03df33bb1b72bbaaf5208a1cddc5ba3
Anup Nehe <anup.nehe@technoforte.co.in>
2023-11-29 15:32:04 +0530

    MOSIP-30482 updated code in release-1.2.0.1 from develop
    Signed-off-by: Anup Nehe <anup.nehe@technoforte.co.in>

---
+++ pmptest/src/main/java/io/mosip/testrig/pmpui/testcase/AdminUploadCaCertTest.java
@@ -0,0 +1,32 @@
+package io.mosip.testrig.pmpui.testcase;
+
+import java.awt.AWTException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import io.mosip.testrig.pmpui.utility.BaseClass;
+import io.mosip.testrig.pmpui.utility.Commons;
+
+
+public class AdminUploadCaCertTest extends BaseClass {
+	
+	@Test(groups = "UFCC",dataProvider = "data-provider-ca")
+	public void adminUploadCaCertTest(String cer) throws InterruptedException, AWTException {
+
+		
+		
+		test=extent.createTest("AdminUploadCaCertTest", "verify Login");
+		Commons.click(test,driver, By.xpath("//a[@href='#/pmp/resources/uploadcacert/upload']"));
+		
+		String dropdwnVal=cer.substring(0, cer.indexOf("_", 0));
+		
+		
+		
+		Commons.uploadPartnerCert(driver,By.id("partnerDomain"),dropdwnVal,"\\ca_cert\\",cer);
+		
+		
+		 
+
+	}
+}
