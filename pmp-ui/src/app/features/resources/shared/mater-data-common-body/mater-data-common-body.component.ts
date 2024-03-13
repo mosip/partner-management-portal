43b15ebf567538fa33285e7b6ac2ed5068e687a3
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2024-03-13 18:11:48 +0530

    MOSIP-31929
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- pmp-ui/src/app/features/resources/shared/mater-data-common-body/mater-data-common-body.component.ts
+++ pmp-ui/src/app/features/resources/shared/mater-data-common-body/mater-data-common-body.component.ts
@@ -44,7 +44,6 @@ export class MaterDataCommonBodyComponent implements OnInit {
   disableForms: boolean;
   copyPrimaryWord: any;
   copySecondaryWord: any;
-  searchResult: any[] = [];
 
   @Input() primaryData: any = {};
   @Input() secondaryData: any;
@@ -132,12 +131,12 @@ export class MaterDataCommonBodyComponent implements OnInit {
       else if(url === "datasharepolicy"){
         this.pageName = "Data Share Policy";        
         this.primaryData = {"name": "", "desc": "", "policies": "", "policyGroupName": "", "policyType": "DataShare", "version": "1.1"};
-        this.getPolicyGroup();
+        this.getPolicyGroup("policyGroupName");
       }
       else if(url === "authpolicy"){
         this.pageName = "Auth Policy";        
         this.primaryData = {"name": "", "desc": "", "policies": "", "policyGroupName": "", "policyType": "Auth", "version": "1.1"};
-        this.getPolicyGroup();
+        this.getPolicyGroup("policyGroupName");
       }
       else if(url === "policymapping"){
         this.pageName = "Map Policy";
@@ -414,9 +413,7 @@ export class MaterDataCommonBodyComponent implements OnInit {
     this.dataStorageService
       .getFiltersForAllDropDown('partnermanager/partners', request)
       .subscribe(response => {
-        if(response.response.filters)   
-        this.searchResult = response.response.filters.sort((a, b) => (a.name && b.name) ? a.id.localeCompare(b.name) : 0);
-        this.dropDownValues.partnerTypeCode.primary = response.response.filters.sort((a, b) => (a.name && b.name) ? a.name.localeCompare(b.name) : 0);
+        this.dropDownValues[key] = response.response.filters;
       });
   }
 
@@ -459,17 +456,9 @@ export class MaterDataCommonBodyComponent implements OnInit {
       });
   }
 
-  onKey(value) {
-    this.searchResult = this.search(value);
-  }
 
-  search(value: string) {
-    let filter = value.toLowerCase();
-    return this.dropDownValues.partnerTypeCode.primary.filter(option => option.fieldCode.toLowerCase().startsWith(filter));
-  }
-
-  getPolicyGroup() {
-    const filterObject = new FilterValuesModel('id', 'unique', '');
+  getPolicyGroup(key) {
+    const filterObject = new FilterValuesModel('name', 'unique', '');
     let optinalFilterObject = new OptionalFilterValuesModel('isActive', 'equals', 'true');
     let filterRequest = new FilterRequest([filterObject], this.primaryLang, [optinalFilterObject]);
     filterRequest["purpose"] = "REGISTRATION";
@@ -477,9 +466,7 @@ export class MaterDataCommonBodyComponent implements OnInit {
     this.dataStorageService
       .getFiltersForAllDropDown('policymanager/policies/group', request)
       .subscribe(response => {
-        if(response.response.filters)   
-        this.searchResult = response.response.filters.sort((a, b) => (a.id && b.id) ? a.id.localeCompare(b.id) : 0);
-        this.dropDownValues.partnerTypeCode.primary = response.response.filters.sort((a, b) => (a.id && b.id) ? a.id.localeCompare(b.id) : 0);        
+        this.dropDownValues[key] = response.response.filters;
       });
   }
 
