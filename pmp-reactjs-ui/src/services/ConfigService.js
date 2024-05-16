49bd9f236a803ad607aed9a389337cb5cdfa3b61
Mayura Deshmukh <mayura.deshmukh@gmail.com>
2024-05-16 17:58:10 +0530

    DSD-5197 fix in progress
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>

--- pmp-reactjs-ui/src/services/ConfigService.js
+++ pmp-reactjs-ui/src/services/ConfigService.js
@@ -5,6 +5,7 @@ export const loadAppConfig = async () => {
         //const resp = await HttpService.get("/api/configs");
         //const appConfig = resp.data.response;
         //localStorage.setItem('appConfig', JSON.stringify(appConfig));
+        console.log(window.origin + process.env.REACT_APP_API_BASE_URL);
     } catch (err) {
         console.log("unable to load configs: " + err);
     }
--- pmp-reactjs-ui/src/setupProxy.js
+++ pmp-reactjs-ui/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: process.env.NODE_ENV !== "production" ? process.env.REACT_APP_API_BASE_URL : window.origin + process.env.REACT_APP_API_BASE_URL,
+            target: process.env.NODE_ENV !== "production" ? process.env.REACT_APP_API_BASE_URL : process.env.REACT_APP_API_BASE_URL,
             changeOrigin: false,
             secure: process.env.NODE_ENV !== "production" ? false : true,
             pathRewrite: {
