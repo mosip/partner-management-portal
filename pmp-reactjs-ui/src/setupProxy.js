3be0da42ce782568f6dc3501492b338e823d0c5c
Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>
2024-05-16 18:29:29 +0530

    DSD-5197 deployment issues (#332)
    * fixed nginx
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * DSD-5197 corrected nginx and dockerfile
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * fixed nginx
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * DSD-5197 corrected nginx and dockerfile
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * DSD-5197 fixing deployment
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * DSD-5197 fix in progress
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    * DSD-5197 deployment issues
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    
    ---------
    
    Signed-off-by: Mayura Deshmukh <mayura.deshmukh@gmail.com>
    Signed-off-by: Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>

--- pmp-reactjs-ui/src/setupProxy.js
+++ pmp-reactjs-ui/src/setupProxy.js
@@ -3,9 +3,9 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: process.env.NODE_ENV !== "production" ? process.env.REACT_APP_API_BASE_URL : process.env.REACT_APP_API_BASE_URL,
+            target: process.env.REACT_APP_API_BASE_URL,
             changeOrigin: false,
-            secure: process.env.NODE_ENV !== "production" ? false : true,
+            secure: process.env.REACT_APP_API_BASE_URL_SECURE,
             pathRewrite: {
                 "^/api": ""
             }
