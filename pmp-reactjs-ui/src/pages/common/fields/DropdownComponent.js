b40b54e001e5c28251608a22bd38bb98873ee333
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-05-27 14:46:50 +0530

    MOSIP-32957: Filter dropdown fixes
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/fields/DropdownComponent.js
+++ pmp-reactjs-ui/src/pages/common/fields/DropdownComponent.js
@@ -1,21 +1,20 @@
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-function DropdownComponent({ fieldName, dropdownDataList, onDropDownChangeEvent, fieldNameKey, dropDownPlaceHolder }) {
+function DropdownComponent({ fieldName, dropdownDataList, onDropDownChangeEvent, fieldNameKey, dropDownPlaceHolder, openDropdown, setOpenDropdown }) {
 
     const { t } = useTranslation();
 
     const [selectedDropdownEntry, setSelectedDropdownEntry] = useState("");
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const changeDropdownSelection = (selectedid) => {
         setSelectedDropdownEntry(selectedid);
-        setIsDropdownOpen(false);
+        setOpenDropdown(null);
         onDropDownChangeEvent(fieldName, selectedid);
     };
-    const openDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
+    const clickOnDropdown = () => {
+        setOpenDropdown(fieldName);
     };
 
     return (
@@ -24,29 +23,31 @@ function DropdownComponent({ fieldName, dropdownDataList, onDropDownChangeEvent,
                 {t(fieldNameKey)}:
             </label>
             <div className="relative w-full">
-                <button onClick={openDropdown} className="flex items-center justify-between w-[282px] h-10 px-2 py-2 border border-gray-400 bg-white rounded-[4px] text-base text-[15px] text-gray-800 leading-tight focus:outline-none focus:shadow-none" type="button">
+                <button onClick={clickOnDropdown} className="flex items-center justify-between w-[282px] h-10 px-2 py-2 border border-gray-400 bg-white rounded-[4px] text-base text-[15px] text-gray-800 leading-tight focus:outline-none focus:shadow-none" type="button">
                     <span>{
                         selectedDropdownEntry ?
                         dropdownDataList.map(dropdownItem => { return (selectedDropdownEntry === dropdownItem.fieldValue ? dropdownItem.fieldCode : '') })
                         : t(dropDownPlaceHolder)}
                     </span>
-                    <svg className={`w-3 h-2 ml-3 transform ${isDropdownOpen ? 'rotate-180' : 'rotate-0'} text-gray-500 text-sm`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <svg className={`w-3 h-2 ml-3 transform ${openDropdown === fieldName ? 'rotate-180' : 'rotate-0'} text-gray-500 text-sm`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                     </svg>
                 </button>
-                {isDropdownOpen && (
+                {openDropdown === fieldName && (
                     <div className="absolute z-50 top-10 left-0  w-full">
                         <div className="z-10 border border-gray-400 scroll-auto bg-white rounded-md shadow-lg w-full dark:bg-gray-700 cursor-pointer">
                             <div className="max-h-40 overflow-y-auto">
                                 {dropdownDataList.map((dropdownItem, index) => {
                                     return (
-                                        <div key={index} className="min-h-3">
-                                            <button
-                                                className={`block w-full px-4 py-2 text-left text-base text-blue-950
-                                                    ${selectedDropdownEntry === dropdownItem.fieldCode ? 'bg-gray-100' : 'hover:bg-gray-100'}`}
-                                                onClick={() => changeDropdownSelection(dropdownItem.fieldValue)}>
-                                                {dropdownItem.fieldCode}
-                                            </button>
+                                        <div key={index}>
+                                            {dropdownItem.fieldCode !== "" && (
+                                                <button
+                                                    className={`block w-full px-4 py-1 text-left text-base text-blue-950
+                                                        ${selectedDropdownEntry === dropdownItem.fieldCode ? 'bg-gray-100' : 'hover:bg-gray-100'}`}
+                                                    onClick={() => changeDropdownSelection(dropdownItem.fieldValue)}>
+                                                    {dropdownItem.fieldCode}
+                                                </button>
+                                            )}
                                             <div className="border-gray-200 border-t mx-2"></div>
                                         </div>
                                     )
--- pmp-reactjs-ui/src/pages/policies/PoliciesFilter.js
+++ pmp-reactjs-ui/src/pages/policies/PoliciesFilter.js
@@ -8,6 +8,11 @@ function PoliciesFilter({ filteredPoliciesList, onFilterChange }) {
     const [policyGroupData, setPolicyGroupData] = useState([]);
     const [policyNameData, setPolicyNameData] = useState([]);
     const [statusData, setStatusData] = useState([]);
+    const [openDropdown, setOpenDropdown] = useState(null);
+
+    const handleOpenDropdown = (fieldName) => {
+        setOpenDropdown(openDropdown === fieldName ? null : fieldName);
+    };
 
     useEffect(() => {
         const getData = (fieldName) => {
@@ -50,11 +55,11 @@ function PoliciesFilter({ filteredPoliciesList, onFilterChange }) {
     return (
         <>
             <div className="flex w-full p-2 justify-start bg-gray-50 flex-wrap">
-                <DropdownComponent fieldName='partnerId' dropdownDataList={partnerIdData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.partnerId' dropDownPlaceHolder='policies.selectPartnerId'></DropdownComponent>
-                <DropdownComponent fieldName='partnerType' dropdownDataList={partnerTypeData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.partnerType' dropDownPlaceHolder='policies.selectPartnerType'></DropdownComponent>
-                <DropdownComponent fieldName='policyGroup' dropdownDataList={policyGroupData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.policyGroup' dropDownPlaceHolder='policies.selectPolicyGroup'></DropdownComponent>
-                <DropdownComponent fieldName='policyName' dropdownDataList={policyNameData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.policyName' dropDownPlaceHolder='policies.selectPolicyName'></DropdownComponent>
-                <DropdownComponent fieldName='status' dropdownDataList={statusData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.status' dropDownPlaceHolder='policies.selectStatus'></DropdownComponent>
+                <DropdownComponent fieldName='partnerId' dropdownDataList={partnerIdData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.partnerId' dropDownPlaceHolder='policies.selectPartnerId' openDropdown={openDropdown} setOpenDropdown={handleOpenDropdown}></DropdownComponent>
+                <DropdownComponent fieldName='partnerType' dropdownDataList={partnerTypeData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.partnerType' dropDownPlaceHolder='policies.selectPartnerType' openDropdown={openDropdown} setOpenDropdown={handleOpenDropdown}></DropdownComponent>
+                <DropdownComponent fieldName='policyGroup' dropdownDataList={policyGroupData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.policyGroup' dropDownPlaceHolder='policies.selectPolicyGroup' openDropdown={openDropdown} setOpenDropdown={handleOpenDropdown}></DropdownComponent>
+                <DropdownComponent fieldName='policyName' dropdownDataList={policyNameData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.policyName' dropDownPlaceHolder='policies.selectPolicyName' openDropdown={openDropdown} setOpenDropdown={handleOpenDropdown}></DropdownComponent>
+                <DropdownComponent fieldName='status' dropdownDataList={statusData} onDropDownChangeEvent={onFilterChangeEvent} fieldNameKey='policies.status' dropDownPlaceHolder='policies.selectStatus' openDropdown={openDropdown} setOpenDropdown={handleOpenDropdown}></DropdownComponent>
             </div>
         </>
     );
