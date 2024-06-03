2546b65e3d6a9b5b55b31854197b525dcd2cf306
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-06-03 20:29:38 +0530

    MOSIP-32711: Added submit flow in request policy (#408)
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/policies/PoliciesFilter.js
+++ pmp-reactjs-ui/src/pages/policies/PoliciesFilter.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import DropdownComponent from '../common/fields/DropdownComponent.js';
 import { useTranslation } from 'react-i18next';
-import { createDropdownDataList } from '../../utils/AppUtils.js';
+import { getPartnerTypeDescription, getStatusCode } from '../../utils/AppUtils.js';
 
 function PoliciesFilter({ filteredPoliciesList, onFilterChange }) {
     const { t } = useTranslation();
@@ -13,12 +13,46 @@ function PoliciesFilter({ filteredPoliciesList, onFilterChange }) {
     const [statusData, setStatusData] = useState([]);
 
     useEffect(() => {
+        const getData = (fieldName) => {
+            let dataArr = [];
+            dataArr.push({
+                fieldCode: "",
+                fieldValue: ""
+            });
+            filteredPoliciesList.forEach(item => {
+                let alreadyAdded = false;
+                dataArr.forEach(item1 => {
+                    if (item1.fieldValue === item[fieldName]) {
+                        alreadyAdded = true;
+                    }
+                });
+                if (!alreadyAdded) {
+                    if (fieldName === "partnerType") {
+                        dataArr.push({
+                            fieldCode: getPartnerTypeDescription(item[fieldName], t),
+                            fieldValue: item[fieldName]
+                        });
+                    } else if (fieldName === "status") {
+                        dataArr.push({
+                            fieldCode: getStatusCode(item[fieldName], t),
+                            fieldValue: item[fieldName]
+                        });
+                    } else {
+                        dataArr.push({
+                            fieldCode: item[fieldName],
+                            fieldValue: item[fieldName]
+                        });
+                    }
+                }
+            });
+            return dataArr;
+        }
         const fetchData = async () => {
-            setPartnerIdData(createDropdownDataList('partnerId', filteredPoliciesList, t));
-            setPartnerTypeData(createDropdownDataList('partnerType', filteredPoliciesList, t));
-            setPolicyGroupNameData(createDropdownDataList('policyGroupName', filteredPoliciesList, t));
-            setPolicyNameData(createDropdownDataList('policyName', filteredPoliciesList, t));
-            setStatusData(createDropdownDataList('status', filteredPoliciesList, t));
+            setPartnerIdData(getData('partnerId'));
+            setPartnerTypeData(getData('partnerType'));
+            setPolicyGroupNameData(getData('policyGroupName'));
+            setPolicyNameData(getData('policyName'));
+            setStatusData(getData('status'));
         };
         fetchData();
     }, [filteredPoliciesList, t]);
