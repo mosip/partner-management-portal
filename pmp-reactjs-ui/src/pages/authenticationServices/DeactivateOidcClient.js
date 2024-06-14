c09e73ee9aa0b0835df7ea5bca4228f86fa26eb2
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-06-14 17:53:22 +0530

    MOSIP-33245: Ui development of deactivate oidc client
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/authenticationServices/DeactivateOidcClient.js
+++ pmp-reactjs-ui/src/pages/authenticationServices/DeactivateOidcClient.js
@@ -1,68 +1,58 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState} from "react";
 import { useTranslation } from "react-i18next";
-import { getUserProfile } from "../../services/UserProfileService";
-import { isLangRTL } from "../../utils/AppUtils";
-import backArrow from "../../svg/back_arrow.svg";
-import { formatDate, moveToOidcClientsList } from "../../utils/AppUtils";
+import LoadingIcon from "../common/LoadingIcon";
+import ErrorMessage from "../common/ErrorMessage";
 
-function DeactivateOidcClient() {
+function DeactivateOidcClient({ closePopUp, clientData }) {
     const { t } = useTranslation();
-    const isLoginLanguageRTL = isLangRTL(getUserProfile().langCode);
-    const navigate = useNavigate();
-    const [oidcClientDetails, setOidcClientDetails] = useState([]);
+    const [errorCode, setErrorCode] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [dataLoaded, setDataLoaded] = useState(true);
 
-    useEffect(() => {
-        const clientData = localStorage.getItem('selectedClientData');
-        if (clientData) {
-            try {
-                const selectedClient = JSON.parse(clientData);
-                setOidcClientDetails(selectedClient);
-            } catch (error) {
-                navigate('/partnermanagement/authenticationServices/oidcClientsList');
-                console.error('Error in viewOidcClientDetails page :', error);
-            }
-        } else {
-            navigate('/partnermanagement/authenticationServices/oidcClientsList');
-        }
-    }, [navigate]);
-
-    const moveToHome = () => {
-        navigate("/partnermanagement");
+    const cancelErrorMsg = () => {
+        setErrorMsg("");
     };
 
+    const clickOnSubmit = async () => {
+        window.location.reload();
+    }
+
+    const styles = {
+        loadingDiv: "!py-[35%]"
+    }
+
     return (
-        <>
-            <div className={`flex-col w-full p-5 bg-anti-flash-white h-full font-inter mb-[2%] ${isLoginLanguageRTL ? "mr-[8%]" : "ml-[8%]"} overflow-x-scroll`}>
-                <div className="flex justify-between mb-5">
-                    <div className="flex items-center gap-x-2">
-                        <img
-                            src={backArrow}
-                            alt=""
-                            onClick={() => moveToOidcClientsList(navigate)}
-                            className={`cursor-pointer ${isLoginLanguageRTL ? "rotate-180" : null}`}
-                        />
-                        <div className="flex-col">
-                            <h1 className="font-bold text-lg text-md text-dark-blue">
-                                {t("deactivateOidcClient.deactivateOidcClient")}
-                            </h1>
-                            <div className="flex space-x-1">
-                                <p
-                                    onClick={() => moveToHome()}
-                                    className="font-semibold text-tory-blue text-xs cursor-pointer"
-                                >
-                                    {t("commons.home")} /
-                                </p>
-                                <p onClick={() => moveToOidcClientsList(navigate)} className="font-semibold text-tory-blue text-xs cursor-pointer">
-                                    {t("deactivateOidcClient.authenticationServiceSection")}
-                                </p>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-[16%] z-50 font-inter cursor-default">
+            <div className={`bg-white md:w-[402px] w-[50%] mx-auto rounded-lg shadow-lg h-[272px]`}>
+                {!dataLoaded && (
+                    <LoadingIcon styleSet={styles}></LoadingIcon>
+                )}
+                {dataLoaded && (
+                    <>
+                        {errorMsg && (
+                            <div className="flex justify-end absolute w-1/3">
+                                <div className="flex justify-between items-center min-h-14 bg-[#C61818] rounded-xl p-3">
+                                    <ErrorMessage errorCode={errorCode} errorMessage={errorMsg} clickOnCancel={cancelErrorMsg}></ErrorMessage>
+                                </div>
+                            </div>
+                        )}
+                        <div className={`p-[10%] flex-col text-center justify-center items-center`}>
+                            <p className="text-[17px] font-semibold text-black break-words px-[6%]">
+                                {t('deactivateOidcClient.oidcClientName')} - '{clientData.oidcClientName}'?
+                            </p>
+                            <p className="text-sm text-[#666666] break-words py-[6%]">
+                                {t('deactivateOidcClient.description')}
+                            </p>
+                            <div className="flex flex-row items-center justify-center space-x-3 pt-[4%]">
+                                <button onClick={() => closePopUp(false)} type="button" className="w-40 h-12 border-[#1447B2] border rounded-md text-tory-blue text-base font-semibold">{t('requestPolicy.cancel')}</button>
+                                <button onClick={() => clickOnSubmit()}type="button" className="w-40 h-12 border-[#1447B2] border rounded-md bg-tory-blue text-white text-base font-semibold">{t('requestPolicy.submit')}</button>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </>
+                )}
             </div>
-        </>
+        </div>
     )
 }
 
