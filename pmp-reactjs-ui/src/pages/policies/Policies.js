b831035e9fbee1c83cbb68422a6183d9e210900e
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-06-21 12:47:48 +0530

    MOSIP-33501 : UI bug fix. (#507)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-reactjs-ui/src/pages/policies/Policies.js
+++ pmp-reactjs-ui/src/pages/policies/Policies.js
@@ -375,7 +375,7 @@ function Policies() {
                           <tbody>
                             {tableRows.map((partner, index) => {
                               return (
-                                <tr key={index} className={`border-t border-[#E5EBFA] cursor-pointer text-[0.8rem] text-[#191919] font-medium ${partner.status.toLowerCase() === "deactivated" ? "text-[#969696]" : "text-[#191919]"}`}>
+                                <tr key={index} className={`border-t border-[#E5EBFA] relative cursor-pointer text-[0.8rem] text-[#191919] font-medium ${partner.status.toLowerCase() === "deactivated" ? "text-[#969696]" : "text-[#191919]"}`}>
                                   <td onClick={() => showViewPolicyDetails(partner)} className="px-2">{partner.partnerId}</td>
                                   <td onClick={() => showViewPolicyDetails(partner)} className="px-2">{getPartnerTypeDescription(partner.partnerType, t)}</td>
                                   <td onClick={() => showViewPolicyDetails(partner)} className="px-2">{partner.policyGroupName}</td>
