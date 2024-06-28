43083ef72bed443ed4137ff756911db63f89b357
sudeep <sudeep.j7353@gmail.com>
2024-06-28 14:23:42 +0530

    MOSIP:33975 MOSIP:33976 - Exceptional handling.
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-reactjs-ui/src/services/HttpService.js
+++ pmp-reactjs-ui/src/services/HttpService.js
@@ -5,11 +5,12 @@ import { setUserProfile, getUserProfile } from './UserProfileService.js';
 
 export const HttpService = axios.create({
   withCredentials: true,
-  baseURL: process.env.NODE_ENV !== 'production'? '' : window._env_.REACT_APP_PARTNER_MANAGER_API_BASE_URL
+  baseURL: process.env.NODE_ENV !== 'production' ? '' : window._env_.REACT_APP_PARTNER_MANAGER_API_BASE_URL,
+  timeout: 120000
 })
 
 export const setupResponseInterceptor = (navigate) => {
-  
+
   HttpService.interceptors.response.use((response) => { // block to handle success case
     const originalRequestUrl = response.config.url;
     //console.log("interceptor: " + originalRequestUrl);
@@ -26,7 +27,7 @@ export const setupResponseInterceptor = (navigate) => {
           "phoneNumber": userData.phoneNumber,
           "orgName": userData.organizationName,
           "partnerType": userData.partnerType,
-          "langCode": userData.locale ? userData.locale: 'eng',
+          "langCode": userData.locale ? userData.locale : 'eng',
           "roles": resp.role
         };
         setUserProfile(profile);
@@ -37,24 +38,33 @@ export const setupResponseInterceptor = (navigate) => {
     //in case user has a new started session on any page other than dashboard 
     //and he is not a registered user, then we want to forecfully redirect him to dashboard 
     //where he will be forced to select a policy group
-    if (originalRequestUrl.split('/').includes('verify')) {  
+    if (originalRequestUrl.split('/').includes('verify')) {
       const emailResp = response.data.response;
       const reqUrl = window.location.href.split('/');
       if (!emailResp.emailExists && !reqUrl.includes("dashboard")) {
         window.location.href = '/';
-      }  
-    }  
+      }
+    }
     return response;
   },
     (error) => { // block to handle error case
-      if (error.response && (error.response.status === 401
-        || error.response.status === 403)) { 
-        // Code inside this block will refresh the auth token  
-        console.log(error);
-        let redirectUrl = process.env.NODE_ENV !== 'production'? '' : window._env_.REACT_APP_PARTNER_MANAGER_API_BASE_URL; 
-        redirectUrl = redirectUrl + getLoginRedirectUrl(window.location.href);
-        console.log(redirectUrl);
-        window.location.href = redirectUrl;
+      if (error.code === 'ECONNABORTED') {
+        // Handle timeout error
+        navigate('/partnermanagement/runtimeError', { state: { messageType: 'timeout', errorCode: '408', errorText: 'Request timed out' } });
+      } else if (error.response) {
+        if (error.response.status === 401) {
+          console.log(error);
+          let redirectUrl = process.env.NODE_ENV !== 'production' ? '' : window._env_.REACT_APP_PARTNER_MANAGER_API_BASE_URL;
+          redirectUrl = redirectUrl + getLoginRedirectUrl(window.location.href);
+          console.log(redirectUrl);
+          window.location.href = redirectUrl;
+        } else if (error.response.status === 403) {
+          navigate('/partnermanagement/runtimeError', { state: { messageType: 'noAccess', errorCode: error.response.status, errorText: error.response.statusText } });
+        } else {
+          navigate('/partnermanagement/runtimeError', { state: { messageType: 'somethingWentWrong', errorCode: error.response.status, errorText: error.response.statusText } });
+        }
+      } else {
+        navigate('/partnermanagement/runtimeError');
       }
       return Promise.reject(error);
     });
