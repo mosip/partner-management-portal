701370480f8d02d202d201e24df41885e8491e29
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-07-05 17:58:41 +0530

    MOSIP-34246: Fixed the  policy details alignment from filter in polices screen
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/fields/DropdownComponent.js
+++ pmp-reactjs-ui/src/pages/common/fields/DropdownComponent.js
@@ -57,7 +57,7 @@ function DropdownComponent({ fieldName, dropdownDataList, onDropDownChangeEvent,
             <div className="relative w-full" ref={dropdownRef}>
                 <button onClick={openDropdown} disabled={disabled} className={`flex items-center justify-between w-fit h-10 px-2 py-2 border border-[#707070] bg-white rounded-[4px] text-[15px] ${selectedDropdownEntry ? 'text-[#343434]' : 'text-grayish-blue'} leading-tight focus:outline-none
                     focus:shadow-none overflow-x-auto whitespace-nowrap no-scrollbar ${(styleSet && styleSet.dropdownButton) ? styleSet.dropdownButton : ''}`} type="button">
-                    <span>{
+                    <span className='break-all break-words text-wrap'>{
                         selectedDropdownEntry ?
                         dropdownDataList.map(dropdownItem => { return (selectedDropdownEntry === dropdownItem.fieldValue ? dropdownItem.fieldCode : '') })
                         : t(placeHolderKey)}
