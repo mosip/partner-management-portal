66a0cecc2b55e40ad27ab9eba96ce879a0c84024
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-07-08 15:39:37 +0530

    MOSIP-34166: Updated ASC and DESC sort icons and highlighted the descending sort icon in createDate col
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/policies/Policies.js
+++ pmp-reactjs-ui/src/pages/policies/Policies.js
@@ -27,7 +27,7 @@ function Policies() {
   const [policiesList, setPoliciesList] = useState([]);
   const [filteredPoliciesList, setFilteredPoliciesList] = useState([]);
   const [order, setOrder] = useState("ASC");
-  const [activeSortAsc, setActiveSortAsc] = useState("");
+  const [activeSortAsc, setActiveSortAsc] = useState("createDate");
   const [activeSortDesc, setActiveSortDesc] = useState("");
   const [firstIndex, setFirstIndex] = useState(0);
   const [selectedRecordsPerPage, setSelectedRecordsPerPage] = useState(8);
@@ -305,17 +305,17 @@ function Policies() {
                                       {t(header.headerNameKey)}
                                       {header.id !== "action" && (
                                         <div>
-                                          <svg className="cursor-pointer mx-2 mb-0.5" onClick={() => sortAscOrder(header.id)} alt="Ascending"
+                                          <svg className="cursor-pointer mx-2 mb-0.5" onClick={() => sortDescOrder(header.id)} alt="Descending"
                                             xmlns="http://www.w3.org/2000/svg"
                                             width="8" height="8" viewBox="0 0 7 6">
                                             <path id="Polygon_3" data-name="Polygon 3" d="M2.636,1.481a1,1,0,0,1,1.728,0L6.123,4.5A1,1,0,0,1,5.259,6H1.741A1,1,0,0,1,.877,4.5Z"
-                                              fill={`${(activeSortAsc === header.id && order === "ASC") ? "#1447b2" : "#969696"}`} />
+                                              fill={`${(activeSortDesc === header.id && order === "DESC") ? "#1447b2" : "#969696"}`} />
                                           </svg>
-                                          <svg className="cursor-pointer mx-2" onClick={() => sortDescOrder(header.id)} alt="Descending"
+                                          <svg className="cursor-pointer mx-2" onClick={() => sortAscOrder(header.id)} alt="Ascending"
                                             xmlns="http://www.w3.org/2000/svg"
                                             width="8" height="8" viewBox="0 0 7 6">
                                             <path id="Polygon_4" data-name="Polygon 4" d="M2.636,1.481a1,1,0,0,1,1.728,0L6.123,4.5A1,1,0,0,1,5.259,6H1.741A1,1,0,0,1,.877,4.5Z"
-                                              transform="translate(7 6) rotate(180)" fill={`${(activeSortDesc === header.id && order === "DESC") ? "#1447b2" : "#969696"}`} />
+                                              transform="translate(7 6) rotate(180)" fill={`${(activeSortAsc === header.id && order === "ASC") ? "#1447b2" : "#969696"}`} />
                                           </svg>
                                         </div>
                                       )}
