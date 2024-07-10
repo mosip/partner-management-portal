2ce1c675b4c568e513a293a697e746adb107302f
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-07-10 14:50:24 +0530

    MOSIP-34329: Adjusted the Alignment issue of headers in the table (#577)
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/authenticationServices/OidcClientsList.js
+++ pmp-reactjs-ui/src/pages/authenticationServices/OidcClientsList.js
@@ -325,7 +325,7 @@ function OidcClientsList() {
                                                 <tr>
                                                     {tableHeaders.map((header, index) => {
                                                         return (
-                                                            <th key={index} className={`py-4 text-xs font-medium text-[#6F6E6E] lg:w-[14%] ${header.id === "policyName" && isLoginLanguageRTL?'pr-4':'pl-4'} ${header.id === "crDtimes" &&isLoginLanguageRTL?'pr-9':'pl-9'} ${header.id === "status" && isLoginLanguageRTL?'pr-12':'pl-12'} ${header.id === "oidcClientId" && 'pr-2'}`}>
+                                                            <th key={index} className={`py-4 text-xs font-medium text-[#6F6E6E] lg:w-[14%] ${header.id === "policyName" && 'pl-4'} ${header.id === "crDtimes" && 'pl-9'} ${header.id === "status" && 'pl-12'} ${header.id === "oidcClientId" && 'pr-2'}`}>
                                                                 <div className="flex gap-x-1 items-center">
                                                                     {t(header.headerNameKey)}
                                                                     {(header.id !== "action") && (header.id !== "oidcClientId") && (
