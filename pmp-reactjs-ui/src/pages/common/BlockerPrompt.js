12a8a62b0b93d304549f3ec293c7b2ec0eb64649
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-07-18 12:43:09 +0530

    MOSIP-34511 : UI bug fix (#611)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-reactjs-ui/src/pages/common/BlockerPrompt.js
+++ pmp-reactjs-ui/src/pages/common/BlockerPrompt.js
@@ -6,9 +6,9 @@ const BlockerPrompt = ({ blocker }) => {
 
   return blocker.state === "blocked" ? (
     <div className="fixed min-w-36 h-full inset-0 w-full flex flex-col z-50 font-inter">
-      <div className="bg-white w-fit mx-auto rounded-xl justify-center shadow-lg p-2 pt-4 text-sm">
+      <div className="bg-white w-fit mx-auto rounded-xl justify-center shadow-lg p-3 pt-4 text-sm">
         <p className="text-center">{t('blockerMessage.description')}</p>
-        <div className="pt-2">
+        <div className="flex justify-center pt-2">
           <button className="w-24 h-9 mx-2 my-1 border-[#1447B2] border rounded-md bg-white text-tory-blue text-sm font-semibold" onClick={() => blocker.proceed()}>
             {t('blockerMessage.proceed')}
           </button>
