7e969b5b967b3fe1ee02cdf17200921272da7f22
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-07-29 18:50:59 +0530

    MOSIP-34329: Sonar bug fix (#659)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-reactjs-ui/src/pages/authenticationServices/AuthenticationServicesTab.js
+++ pmp-reactjs-ui/src/pages/authenticationServices/AuthenticationServicesTab.js
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import { getUserProfile } from '../../services/UserProfileService';
 import { isLangRTL, onPressEnterKey } from '../../utils/AppUtils';
 
-
 function AuthenticationServicesTab( {activeOidcClient, setActiveOicdClient, activeApiKey, setActiveApiKey} ) {
 
     const { t } = useTranslation();
@@ -26,17 +25,17 @@ function AuthenticationServicesTab( {activeOidcClient, setActiveOicdClient, acti
     return (
         <div className='flex text-xs bg-[#FCFCFC] font-bold space-x-16 items-start rounded-lg px-[1.5%] pt-[2%]'>
             <div className={`flex-col justify-center`}>
-                <h6 onClick={() => changeToOidcClients()}
+                <h6 onClick={changeToOidcClients}
                     className={`${activeOidcClient ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] ${isLoginLanguageRTL ? "mr-[10%]" : "ml-[10%]"} cursor-pointer text-sm`}
-                    tabIndex="0" onKeyPress={(e)=>onPressEnterKey(e,changeToOidcClients())}>
+                    tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, changeToOidcClients)}>
                     {t('authenticationServices.oidcClient')}
                 </h6>
                 <div className={`h-1 w-24 ${activeOidcClient ? "bg-tory-blue" : "bg-transparent"}  rounded-t-md`}></div>
             </div>
             <div className={`flex-col justify-center`}>
-                <h6 onClick={() => changeToApiKey()}
+                <h6 onClick={changeToApiKey}
                     className={`${activeApiKey ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] ${isLoginLanguageRTL ? "mr-[20%]" : "ml-[20%]"} cursor-pointer text-sm`}
-                    tabIndex="0" onKeyPress={(e)=>onPressEnterKey(e,changeToApiKey())}>
+                    tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, changeToApiKey)}>
                     {t('authenticationServices.apiKey')}
                 </h6>
                 <div className={`h-1 w-24 ${activeApiKey ? "bg-tory-blue" : "bg-transparent"} rounded-t-md`}></div>
