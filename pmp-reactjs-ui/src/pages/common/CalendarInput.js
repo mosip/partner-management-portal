c9ae2e3cbdef8be89c3df08480f5c0878d2ac8a5
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-08-02 19:06:03 +0530

    MOSIP-34613: Implemente Info icon at expiry date label
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/CalendarInput.js
+++ pmp-reactjs-ui/src/pages/common/CalendarInput.js
@@ -5,8 +5,9 @@ import { isLangRTL, handleMouseClickForDropdown } from '../../utils/AppUtils';
 import { getUserProfile } from '../../services/UserProfileService';
 import { useTranslation } from 'react-i18next';
 import calendar_icon from '../../svg/calendar_icon.svg'
+import Information from './fields/Information';
 
-function CalendarInput({ showCalendar, setShowCalender, label, onChange, value, styles }) {
+function CalendarInput({ showCalendar, addInfoIcon, setShowCalender, label, onChange, value, styles }) {
 
   const { t } = useTranslation();
   const isLoginLanguageRTL = isLangRTL(getUserProfile().langCode);
@@ -24,7 +25,12 @@ function CalendarInput({ showCalendar, setShowCalender, label, onChange, value, 
 
   return (
     <div className="flex flex-col w-[48%] overflow-x-auto">
-      <label className={`block text-dark-blue text-sm font-semibold mb-1 ${isLoginLanguageRTL ? "mr-1" : "ml-1"}`}>{label}</label>
+      <label className={`flex items-center gap-x-1 text-dark-blue text-sm font-semibold mb-1 ${isLoginLanguageRTL ? "mr-1" : "ml-1"}`}>
+        <p className={`mb-0.5`}>{label}</p>
+        {addInfoIcon && (
+          <Information infoKey={t('addSbis.addSbiInfoKey')} />
+        )}
+      </label>
       <div className={`flex items-center justify-between h-10 px-2 border border-[#707070] rounded-md text-md text-dark-blue bg-white text-start cursor-pointer`}>
         <span className="text-start ">
           {value.toLocaleDateString() ? value.toLocaleDateString()
