19c0be5804ecc8b9d862ff03298c41dc2b589649
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-08-07 18:06:35 +0530

    MOSIP-34613: Given limit for the Binary hash input
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/fields/Information.js
+++ pmp-reactjs-ui/src/pages/common/fields/Information.js
@@ -16,7 +16,7 @@ function Information({ infoKey }) {
     },[tooltipRef])
     return (
         <div>
-            <div className="flex items-center">
+            <div className="relative flex items-center">
                 <img src={infoIcon} onClick={() => setShowTooltip(!showTooltip)} alt="" className={`cursor-pointer h-[13px] w-[13px] ml-1`} />
                 {showTooltip && (
                     <div ref={tooltipRef} className={`absolute z-20 p-4 w-[20vw] max-w-[300px] max-h-[20vh] overflow-y-auto max-[800px]:h-32 max-[800px]:w-32 shadow-lg bg-white border border-gray-300 rounded ${isLoginLanguageRTL ? 'right-6' : 'left-6'} mt-2`}>
