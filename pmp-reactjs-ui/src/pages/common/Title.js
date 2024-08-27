cf0196d1d294aa719e5bd4452eb11da4a4e368f1
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-08-27 18:25:57 +0530

    MOSIP-35328: Fixed back arrow button alignment for lengthy SBI Version in List of Devices page.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/Title.js
+++ pmp-reactjs-ui/src/pages/common/Title.js
@@ -24,10 +24,10 @@ function Title({ title, subTitle, subTitle2, backLink, backLink2, status, versio
                 <h1 className="font-semibold text-lg text-dark-blue">{t(title)}</h1>
             </div>
 
-            <div className={`flex-col w-full ${status && version ? (!isLoginLanguageRTL ? 'ml-[3%]' : 'mr-[3%]') :(!isLoginLanguageRTL ? 'ml-7' : 'mr-7')}`}>
+            <div className={`flex-col w-full ${!isLoginLanguageRTL ? 'ml-7' : 'mr-7'}`}>
                 {(status && version) && (
-                    <div className="flex mb-1 gap-x-1">
-                        <h1 className={`font-semibold text-lg text-[#707070]`}>{version} |</h1>
+                    <div className="flex mb-1">
+                        <h1 className={`font-semibold text-lg text-[#707070]`}>{version + ' | '}</h1>
                         <div className={`${(status === 'deactivated') ? 'bg-[#A5A5A5] text-white' : bgOfStatus(status)} flex h-fit py-1.5 px-2 ${isLoginLanguageRTL ? "ml-1" : "mr-1 ml-2"} text-xs font-semibold rounded-md`}>
                             {getStatusCode(status, t)}
                         </div>
