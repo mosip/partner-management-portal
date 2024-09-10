19b4acd833cdd669f6023685d0771d64df888790
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-09-10 15:15:20 +0530

    MOSIP-34986: API integration for List of FTM & Approved FTM provider ids (#742)
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/ftmProviderServices/FtmList.js
+++ pmp-reactjs-ui/src/pages/ftmProviderServices/FtmList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { getUserProfile } from '../../services/UserProfileService';
+import { HttpService } from '../../services/HttpService';
 import {
   isLangRTL, handleServiceErrors, getPartnerManagerUrl, formatDate, getStatusCode,
   handleMouseClickForDropdown, toggleSortDescOrder, toggleSortAscOrder, createRequest, bgOfStatus,
@@ -26,7 +27,7 @@ function FtmList() {
   const [filter, setFilter] = useState(false);
   const [selectedRecordsPerPage, setSelectedRecordsPerPage] = useState(8);
   const [order, setOrder] = useState("ASC");
-  const [activeSortAsc, setActiveSortAsc] = useState("crDtimes");
+  const [activeSortAsc, setActiveSortAsc] = useState("createdDateTime");
   const [activeSortDesc, setActiveSortDesc] = useState("");
   const [isDescending, setIsDescending] = useState(false);
   const [firstIndex, setFirstIndex] = useState(0);
@@ -45,41 +46,39 @@ function FtmList() {
   }, [submenuRef]);
 
   useEffect(() => {
-    const tablevalues = [
-      { "partnerId": "P28394091", "make": "Make 01", "model": "Model 01", "crDtimes": "11/10/2025", "certificateExpiryDate": "11/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394092", "make": "Make 02", "model": "Model 02", "crDtimes": "11/10/2025", "certificateExpiryDate": "10/10/2025", "certificateUploadDate": "11/10/2024", "status": "deactivated"},
-      { "partnerId": "P28394093", "make": "Make 03", "model": "Model 03", "crDtimes": "06/10/2025", "certificateExpiryDate": "09/10/2025", "certificateUploadDate": "11/10/2024", "status": "pending_approval"},
-      { "partnerId": "P28394094", "make": "Make 04", "model": "Model 04", "crDtimes": "11/10/2025", "certificateExpiryDate": "12/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394095", "make": "Make 05", "model": "Model 05", "crDtimes": "12/10/2025", "certificateExpiryDate": "13/10/2025", "certificateUploadDate": "11/10/2024", "status": "rejected"},
-      { "partnerId": "P28394096", "make": "Make 06", "model": "Model 06", "crDtimes": "07/10/2025", "certificateExpiryDate": "14/10/2025", "certificateUploadDate": "11/10/2024", "status": "deactivated"},
-      { "partnerId": "P28394097", "make": "Make 07", "model": "Model 07", "crDtimes": "01/10/2025", "certificateExpiryDate": "15/10/2025", "certificateUploadDate": "11/10/2024", "status": "pending_approval"},
-      { "partnerId": "P28394098", "make": "Make 08", "model": "Model 08", "crDtimes": "11/10/2025", "certificateExpiryDate": "16/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394099", "make": "Make 09", "model": "Model 09", "crDtimes": "11/10/2025", "certificateExpiryDate": "17/10/2025", "certificateUploadDate": "11/10/2024", "status": "deactivated"},
-      { "partnerId": "P28394100", "make": "Make 10", "model": "Model 10", "crDtimes": "02/10/2025", "certificateExpiryDate": "18/10/2025", "certificateUploadDate": "11/10/2024", "status": "rejected"},
-      { "partnerId": "P28394101", "make": "Make 11", "model": "Model 11", "crDtimes": "08/10/2025", "certificateExpiryDate": "19/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394102", "make": "Make 12", "model": "Model 12", "crDtimes": "12/10/2025", "certificateExpiryDate": "11/10/2025", "certificateUploadDate": "11/10/2024", "status": "deactivated"},
-      { "partnerId": "P28394103", "make": "Make 13", "model": "Model 13", "crDtimes": "09/10/2025", "certificateExpiryDate": "11/10/2025", "certificateUploadDate": "11/10/2024", "status": "pending_approval"},
-      { "partnerId": "P28394104", "make": "Make 14", "model": "Model 14", "crDtimes": "03/10/2025", "certificateExpiryDate": "01/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394105", "make": "Make 15", "model": "Model 15", "crDtimes": "09/10/2025", "certificateExpiryDate": "02/10/2025", "certificateUploadDate": "11/10/2024", "status": "rejected"},
-      { "partnerId": "P28394106", "make": "Make 16", "model": "Model 16", "crDtimes": "12/10/2025", "certificateExpiryDate": "03/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394107", "make": "Make 17", "model": "Model 17", "crDtimes": "04/10/2025", "certificateExpiryDate": "04/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394108", "make": "Make 18", "model": "Model 18", "crDtimes": "11/10/2025", "certificateExpiryDate": "05/10/2025", "certificateUploadDate": "11/10/2024", "status": "pending_approval"},
-      { "partnerId": "P28394109", "make": "Make 19", "model": "Model 19", "crDtimes": "10/10/2025", "certificateExpiryDate": "06/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394110", "make": "Make 20", "model": "Model 20", "crDtimes": "02/10/2025", "certificateExpiryDate": "07/10/2025", "certificateUploadDate": "11/10/2024", "status": "approved"},
-      { "partnerId": "P28394111", "make": "Make 21", "model": "Model 21", "crDtimes": "05/10/2025", "certificateExpiryDate": "08/10/2025", "certificateUploadDate": "11/10/2024", "status": "pending_approval"},
-      { "partnerId": "P28394112", "make": "Make 22", "model": "Model 22", "crDtimes": "07/10/2025", "certificateExpiryDate": "09/10/2025", "certificateUploadDate": "11/10/2024", "status": "rejected"}
-    ];
-    setFtmList(tablevalues);
-    setFilteredFtmList(tablevalues);
+    const fetchData = async () => {
+      try {
+        setDataLoaded(false);
+        const response = await HttpService.get(getPartnerManagerUrl('/partners/ftm-chip-details', process.env.NODE_ENV));
+        if (response) {
+          const responseData = response.data;
+          if (responseData && responseData.response) {
+            const resData = responseData.response;
+            const sortedData = resData.sort((a, b) => new Date(b.createdDateTime) - new Date(a.createdDateTime));
+            setFtmList(sortedData);
+            setFilteredFtmList(sortedData);
+          } else {
+            handleServiceErrors(responseData, setErrorCode, setErrorMsg);
+          }
+        } else {
+          setErrorMsg(t('ftmList.errorInFtmList'));
+        }
+        setDataLoaded(true);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setErrorMsg(err);
+      }
+    };
+    fetchData();
   }, []);
 
   const tableHeaders = [
     { id: "partnerId", headerNameKey: 'ftmList.partnerId' },
     { id: "make", headerNameKey: "ftmList.make" },
     { id: "model", headerNameKey: "ftmList.model" },
-    { id: "crDtimes", headerNameKey: "ftmList.createdDate" },
-    { id: "certificateExpiryDate", headerNameKey: "ftmList.certificateExpiryDate" },
-    { id: "certificateUploadDate", headerNameKey: "ftmList.certificateUploadDate" },
+    { id: "createdDateTime", headerNameKey: "ftmList.createdDate" },
+    { id: "certificateExpiryDateTime", headerNameKey: "ftmList.certificateExpiryDate" },
+    { id: "certificateUploadDateTime", headerNameKey: "ftmList.certificateUploadDate" },
     { id: "status", headerNameKey: "ftmList.status" },
     { id: "action", headerNameKey: 'ftmList.action' }
   ];
@@ -115,12 +114,12 @@ function FtmList() {
   }
 
   const sortAscOrder = (header) => {
-      const isDateCol = (header === "crDtimes");
+      const isDateCol = (header === "createdDateTime");
       toggleSortAscOrder(header, isDateCol, filteredftmList, setFilteredFtmList, order, setOrder, isDescending, setIsDescending, activeSortAsc, setActiveSortAsc, activeSortDesc, setActiveSortDesc);
   }
 
   const sortDescOrder = (header) => {
-      const isDateCol = (header === "crDtimes");
+      const isDateCol = (header === "createdDateTime");
       toggleSortDescOrder(header, isDateCol, filteredftmList, setFilteredFtmList, order, setOrder, isDescending, setIsDescending, activeSortAsc, setActiveSortAsc, activeSortDesc, setActiveSortDesc);
   }
 
@@ -143,7 +142,7 @@ function FtmList() {
   };
 
   const showManageCertificate = (selectedFtmData) => {
-    if (selectedFtmData.status === "approved") {
+    if (selectedFtmData.status === "approved" || selectedFtmData.status === "pending_cert_upload") {
       navigate('/partnermanagement/ftmChipProviderServices/manageFtmChipCertificate');
     }
   };
@@ -233,9 +232,9 @@ function FtmList() {
                                 <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{ftm.partnerId}</td>
                                 <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{ftm.make}</td>
                                 <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{ftm.model}</td>
-                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{formatDate(ftm.crDtimes, 'date')}</td>
-                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{ftm.certificateExpiryDate}</td>
-                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{ftm.certificateUploadDate}</td>
+                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{formatDate(ftm.createdDateTime, 'date')}</td>
+                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{formatDate(ftm.certificateExpiryDateTime, 'date')}</td>
+                                <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">{formatDate(ftm.certificateUploadDateTime, 'dateTime')}</td>
                                 <td onClick={() => showFtmDetails(ftm)} className="px-2 mx-2">
                                   <div className={`${bgOfStatus(ftm.status)} flex w-fit py-1.5 px-2 my-3 text-xs font-semibold rounded-md`}>
                                     {getStatusCode(ftm.status, t)}
@@ -247,12 +246,12 @@ function FtmList() {
                                       tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => setViewFtmId(index === viewFtmId ? null : index))}>
                                       ...</p>
                                     {viewFtmId === index && (
-                                      <div className={`absolute w-[7%] ${currentArray.length - 1 === index ? '-bottom-2' : currentArray.length - 2 === index ? '-bottom-2' : 'top-7'} z-50 bg-white text-xs font-semibold rounded-lg shadow-md border min-w-fit ${isLoginLanguageRTL ? "left-4 text-right" : "right-4 text-left"}`}>
+                                      <div className={`absolute w-[7%] ${currentArray.length - 1 === index ? '-bottom-2' : currentArray.length - 2 === index ? '-bottom-2' : 'top-7'} z-50 bg-white text-xs font-semibold rounded-lg shadow-md border min-w-fit ${isLoginLanguageRTL ? "left-5 text-right" : "right-5 text-left"}`}>
                                         <p onClick={() => viewFtmDetails(ftm)} className={`py-2 px-4 cursor-pointer text-[#3E3E3E] hover:bg-gray-100 ${isLoginLanguageRTL ? "pl-10" : "pr-10"}`} tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => viewFtmDetails(ftm))}>
                                           {t('ftmList.view')}
                                         </p>
                                         <hr className="h-px bg-gray-200 border-0 mx-1" />
-                                        <p onClick={() => showManageCertificate(ftm)} className={`py-2 px-4 ${isLoginLanguageRTL ? "pl-10" : "pr-10"} ${ftm.status === "approved" ? 'text-[#3E3E3E] cursor-pointer' : 'text-[#A5A5A5] cursor-auto'} hover:bg-gray-100`} tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => showManageCertificate(ftm))}>
+                                        <p onClick={() => showManageCertificate(ftm)} className={`py-2 px-4 ${isLoginLanguageRTL ? "pl-10" : "pr-10"} ${(ftm.status === "approved" || ftm.status === "pending_cert_upload") ? 'text-[#3E3E3E] cursor-pointer' : 'text-[#A5A5A5] cursor-auto'} hover:bg-gray-100`} tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => showManageCertificate(ftm))}>
                                           {t('ftmList.manageCertificate')}
                                         </p>
                                         <hr className="h-px bg-gray-200 border-0 mx-1" />
