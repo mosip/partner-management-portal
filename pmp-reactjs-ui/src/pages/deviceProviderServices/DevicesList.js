2dfb17b7b1cf92990f8fa4ad75c901165ccb37c5
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-09-12 17:21:30 +0530

    MOSIP-35644: Fixed Action menu issue for Pending for Approval & Rejected device once its expanded.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/deviceProviderServices/DevicesList.js
+++ pmp-reactjs-ui/src/pages/deviceProviderServices/DevicesList.js
@@ -322,7 +322,7 @@ function DevicesList() {
                                                                                     tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => setViewDeviceId(index === viewDeviceId ? null : index))}>
                                                                                     ...</p>
                                                                                 {viewDeviceId === index && (
-                                                                                    <div className={`absolute w-[7%] ${currentArray.length - 1 === index ? '-bottom-2' : currentArray.length - 2 === index ? '-bottom-2' : 'top-7'} z-50 bg-white text-xs font-semibold rounded-lg shadow-md border min-w-fit ${isLoginLanguageRTL ? "left-4 text-right" : "right-4 text-left"}`}>
+                                                                                    <div className={`absolute w-[7%] ${currentArray.length - 1 === index ? '-bottom-9' : currentArray.length - 2 === index ? '-bottom-2' : 'top-5'} z-50 bg-white text-xs font-semibold rounded-lg shadow-md border min-w-fit ${isLoginLanguageRTL ? "left-4 text-right" : "right-4 text-left"}`}>
                                                                                         <p onClick={() => viewDeviceDetails(device)} className={`py-2 px-4 cursor-pointer text-[#3E3E3E] hover:bg-gray-100 ${isLoginLanguageRTL ? "pl-10" : "pr-10"}`} tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, () => viewDeviceDetails(device))}>
                                                                                             {t('devicesList.view')}
                                                                                         </p>
