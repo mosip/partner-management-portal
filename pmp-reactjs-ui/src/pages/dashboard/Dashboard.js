383049a2e13936868dfa4e2971543be2b707dc69
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-10-03 12:56:01 +0530

    MOSIP-32722: Enhancement of Partner Admin Home Page/Dashboard
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/dashboard/Dashboard.js
+++ pmp-reactjs-ui/src/pages/dashboard/Dashboard.js
@@ -14,6 +14,8 @@ import policiesIcon from '../../svg/policies_icon.svg';
 import authServiceIcon from '../../svg/auth_services_icon.svg';
 import deviceProviderServices_icon from '../../svg/deviceProviderServices_icon.svg';
 import ftmServicesIcon from "../../svg/ftm_services_icon.svg";
+import pending_requests_icon from '../../svg/pending_requests_icon.svg';
+import sbi_device_details_icon from '../../svg/sbi_device_details_icon.svg';
 import partner_admin_icon from '../../svg/partner_admin_icon.svg';
 import partner_policy_mapping_icon from '../../svg/partner_policy_mapping_icon.svg';
 import ConsentPopup from './ConsentPopup.js';
@@ -77,7 +79,7 @@ function Dashboard() {
         if (getUserProfile().partnerType === "FTM_PROVIDER") {
           setShowFtmServices(true);
         }
-        if (getUserProfile().roles === 'PARTNER_ADMIN') {
+        if (getUserProfile().roles.includes('PARTNER_ADMIN' || 'POLICY_MANAGER') ) {
           setIsPartnerAdmin(true);
         }
         //1. verify that the logged in user's email is registered in PMS table or not
@@ -174,7 +176,7 @@ function Dashboard() {
       {dataLoaded && (
         <>
           {errorMsg && (
-            <ErrorMessage errorCode={errorCode} errorMessage={errorMsg} clickOnCancel={cancelErrorMsg}/>
+            <ErrorMessage errorCode={errorCode} errorMessage={errorMsg} clickOnCancel={cancelErrorMsg} />
           )}
           <div className="mb-6 mt-5 ml-[2%] text-lg font-semibold tracking-tight text-gray-700">
             <p >
@@ -182,20 +184,22 @@ function Dashboard() {
             </p>
           </div>
           <div className="flex mt-2 ml-[3%] flex-wrap break-words">
-            <div id='dashboard_partner_certificated_list_card' onClick={() => partnerCertificatesList()} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, partnerCertificatesList())}>
-              <div className="flex justify-center mb-5">
-                <img src={partnerCertificateIcon} alt="" className="w-8 h-8"></img>
+            {!isPartnerAdmin &&
+              < div id='dashboard_partner_certificated_list_card' onClick={() => partnerCertificatesList()} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, partnerCertificatesList())}>
+                <div className="flex justify-center mb-5">
+                  <img src={partnerCertificateIcon} alt="" className="w-8 h-8"></img>
+                </div>
+                <div>
+                  <h5 className="mb-2 text-sm font-semibold tracking-tight text-gray-600">
+                    {t('dashboard.partnerCertificate')}
+                  </h5>
+                  <p className="mb-3 text-xs font-normal text-gray-400">
+                    {t('dashboard.partnerCertificateDesc')}
+                  </p>
+                </div>
               </div>
-              <div>
-                <h5 className="mb-2 text-sm font-semibold tracking-tight text-gray-600">
-                  {t('dashboard.partnerCertificate')}
-                </h5>
-                <p className="mb-3 text-xs font-normal text-gray-400">
-                  {t('dashboard.partnerCertificateDesc')}
-                </p>
-              </div>
-            </div>
-            {showPolicies && (
+            }
+            {!isPartnerAdmin && showPolicies && (
               <div id='dashboard_policies_card' onClick={() => policies()} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, policies())}>
                 <div className="flex justify-center mb-5">
                   <img src={policiesIcon} alt="" className="w-8 h-8"></img>
@@ -210,7 +214,7 @@ function Dashboard() {
                 </div>
               </div>
             )}
-            {showAuthenticationServices && (
+            {!isPartnerAdmin && showAuthenticationServices && (
               <div id='dashboard_authentication_clients_list_card' onClick={() => moveToOidcClientsList(navigate)} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, moveToOidcClientsList(navigate))}>
                 <div className="flex justify-center mb-5">
                   <img src={authServiceIcon} alt="" className="w-8 h-8"></img>
@@ -225,7 +229,7 @@ function Dashboard() {
                 </div>
               </div>
             )}
-            {showDeviceProviderServices && (
+            {!isPartnerAdmin && showDeviceProviderServices && (
               <div id='dashboard_device_provider_service_card' onClick={deviceProviderServices} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, deviceProviderServices)}>
                 <div className="flex justify-center mb-5">
                   <img src={deviceProviderServices_icon} alt="" className="w-8 h-8" />
@@ -240,7 +244,7 @@ function Dashboard() {
                 </div>
               </div>
             )}
-            {showFtmServices && (
+            {!isPartnerAdmin && showFtmServices && (
               <div id='dashboard_ftm_chip_provider_card' onClick={ftmChipProviderServices} className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, ftmChipProviderServices)}>
                 <div className="flex justify-center mb-5">
                   <img src={ftmServicesIcon} alt="" className="w-8 h-8" />
@@ -259,7 +263,7 @@ function Dashboard() {
               <>
                 <div className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, partnerCertificatesList())}>
                   <div className="flex justify-center mb-5">
-                    <img src={''} alt="" className="w-8 h-8"></img>
+                    <img src={pending_requests_icon} alt="" className="w-8 h-8"></img>
                   </div>
                   <div>
                     <h5 className="mb-2 text-sm font-semibold tracking-tight text-gray-600">
@@ -329,7 +333,7 @@ function Dashboard() {
 
                 <div className="w-[23.5%] min-h-[50%] p-6 mr-3 mb-4 pt-16 bg-white border border-gray-200 shadow cursor-pointer  text-center rounded-xl" tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, partnerCertificatesList())}>
                   <div className="flex justify-center mb-5">
-                    <img src={''} alt="" className="w-8 h-8"></img>
+                    <img src={sbi_device_details_icon} alt="" className="w-8 h-8"></img>
                   </div>
                   <div>
                     <h5 className="mb-2 text-sm font-semibold tracking-tight text-gray-600">
@@ -363,8 +367,9 @@ function Dashboard() {
           {showConsentPopup && (
             <ConsentPopup />
           )}
-        </>)}
-    </div>
+        </>)
+      }
+    </div >
   )
 }
 
