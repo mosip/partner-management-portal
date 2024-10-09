1d8fc4ed0d850536d104be07fb6f4381fc72b959
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-10-09 14:41:34 +0530

    MOSIP-36114: UI Development of Upload Certificate by Partner Admin
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/AppRoutes.js
+++ pmp-reactjs-ui/src/AppRoutes.js
@@ -24,6 +24,7 @@ import FtmList from './pages/partner/ftmProviderServices/FtmList.js';
 import AddFtm from './pages/partner/ftmProviderServices/AddFtm.js';
 import ViewFtmChipDetails from './pages/partner/ftmProviderServices/ViewFtmChipDetails.js';
 import RootTrustCertificateList from './pages/admin/certificates/RootTrustCertificateList.js';
+import UploadRootOfCertificate from './pages/admin/certificates/UploadRootOfCertificate.js';
 
 
 function AppRoutes() {
@@ -128,6 +129,10 @@ function AppRoutes() {
         {
           path: 'admin/certificates/rootTrustCertificateList',
           element: <GuardedRoute><MainLayout><RootTrustCertificateList/></MainLayout></GuardedRoute>
+        },
+        {
+          path: 'admin/certificates/uploadRootOfCertificate',
+          element: <GuardedRoute><MainLayout><UploadRootOfCertificate/></MainLayout></GuardedRoute>
         },
         {
           path: 'runtimeError',
