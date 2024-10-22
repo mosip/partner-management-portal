1722e1b8ecf660095e19fa987b652a4bc1c9ee23
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-10-22 11:59:37 +0530

    MOSIP:36498- UI Development: Server Side Filter (#853)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-reactjs-ui/src/pages/admin/partners/PartnersListFilter.js
+++ pmp-reactjs-ui/src/pages/admin/partners/PartnersListFilter.js
@@ -1,93 +1,41 @@
 import { useState, useEffect } from "react";
 import DropdownComponent from "../../common/fields/DropdownComponent.js";
+import TextInputComponent from "../../common/fields/TextInputComponent.js";
 import { useTranslation } from "react-i18next";
 import { createDropdownData } from "../../../utils/AppUtils.js";
 
 function PartnerListFilter({ filteredPartnersData, onFilterChange }) {
-  console.log(filteredPartnersData)
   const { t } = useTranslation();
-  const [partnerIdData, setPartnerIdData] = useState([]);
-  const [partnerTypeData, setPartnerTypeData] = useState([]);
-  const [organisationNameData, setOrganisationNameData] = useState([]);
-  const [emailIdData, setEmailIdData] = useState([]);
-  const [policyGroupData, setPolicyGroupData] = useState([]);
-  const [statusData, setStatusData] = useState([]);
 
-  console.log(partnerIdData)
-  console.log(partnerTypeData)
+  const [partnerType, setPartnerType] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [certUploadStatus, setCertUploadStatus] = useState([]);
+  const [filters, setFilters] = useState({
+    partnerType: "",
+    status: "",
+    orgName: "",
+    emailID: "",
+    certUploadStatus: "",
+    policyGroup: ""
+  });
+
   useEffect(() => {
-    const fetchData = async () => {
-      setPartnerIdData(
-        createDropdownData(
-          "partnerID",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("partnerList.selectPartnerId")
-        )
-      );
-
-      setPartnerTypeData(
-        createDropdownData(
-          "partnerType",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("partnerList.selectPartnerType")
-        )
-      );
-
-      setOrganisationNameData(
-        createDropdownData(
-          "orgName",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("partnerList.selectOrganisation")
-        )
-      );
-
-      setEmailIdData(
-        createDropdownData(
-          "emailID",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("partnerList.selectEmailAddress")
-        )
-      );
-
-      setPolicyGroupData(
-        createDropdownData(
-          "policyGroup",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("partnerList.selectPolicyGroup")
-        )
-      );
-
-      setStatusData(
-        createDropdownData(
-          "status",
-          "",
-          true,
-          filteredPartnersData,
-          t,
-          t("rootTrustCertificate.selectStatus")
-        )
-      );
-    };
-    fetchData();
-  }, [t]);
+    setPartnerType(
+      createDropdownData("partnerType", "", true, filteredPartnersData, t, t("partnerList.selectPartnerType"))
+    );
+    setStatus(
+      createDropdownData("status", "", true, filteredPartnersData, t, t("partnerList.selectStatus"))
+    );
+    setCertUploadStatus(
+      createDropdownData("certificateUploadStatus", "", true, filteredPartnersData, t, t("partnerList.selectCertUploadStatus"))
+    );
+  }, [filteredPartnersData, t]);
 
   const onFilterChangeEvent = (fieldName, selectedFilter) => {
-    onFilterChange(fieldName, selectedFilter);
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [fieldName]: selectedFilter
+    }));
   };
 
   const styles = {
@@ -95,70 +43,80 @@ function PartnerListFilter({ filteredPartnersData, onFilterChange }) {
   };
 
   return (
-    <>
-      <div className="flex w-full p-2 justify-start bg-[#F7F7F7] flex-wrap">
-        <DropdownComponent
-          fieldName="partnerID"
-          dropdownDataList={partnerIdData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.partnerId"
-          placeHolderKey="partnerList.selectPartnerId"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="partner_id_filter"
-        ></DropdownComponent>
-        <DropdownComponent
-          fieldName="partnerType"
-          dropdownDataList={partnerTypeData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.partnerType"
-          placeHolderKey="partnerList.selectPartnerType"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="partner_type_filter"
-        ></DropdownComponent>
-        <DropdownComponent
-          fieldName="orgName"
-          dropdownDataList={organisationNameData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.organisation"
-          placeHolderKey="partnerList.selectOrganisation"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="partner_organisation_filter"
-        ></DropdownComponent>
-        <DropdownComponent
-          fieldName="emailID"
-          dropdownDataList={emailIdData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.email"
-          placeHolderKey="partnerList.selectEmailAddress"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="email_address_filter"
-        ></DropdownComponent>
-        <DropdownComponent
-          fieldName="policyGroup"
-          dropdownDataList={policyGroupData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.policyGroup"
-          placeHolderKey="partnerList.selectPolicyGroup"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="policy_group_filter"
-        ></DropdownComponent>
-        <DropdownComponent
-          fieldName="status"
-          dropdownDataList={statusData}
-          onDropDownChangeEvent={onFilterChangeEvent}
-          fieldNameKey="partnerList.status"
-          placeHolderKey="partnerList.selectStatus"
-          styleSet={styles}
-          isPlaceHolderPresent={true}
-          id="partner_status_filter"
-        ></DropdownComponent>
+    <div className="flex w-full p-2 justify-start bg-[#F7F7F7] flex-wrap">
+      <TextInputComponent
+        fieldName="partnerId"
+        onTextChange={onFilterChangeEvent}
+        fieldNameKey="partnerList.partnerId"
+        placeHolderKey="partnerList.searchPartnerId"
+        styleSet={styles}
+        id="partner_id_filter"
+      />
+      <DropdownComponent
+        fieldName="partnerType"
+        dropdownDataList={partnerType}
+        onDropDownChangeEvent={onFilterChangeEvent}
+        fieldNameKey="partnerList.partnerType"
+        placeHolderKey="partnerList.selectPartnerType"
+        styleSet={styles}
+        isPlaceHolderPresent={true}
+        id="partner_type_filter"
+      />
+      <TextInputComponent
+        fieldName="orgName"
+        onTextChange={onFilterChangeEvent}
+        fieldNameKey="partnerList.organisation"
+        placeHolderKey="partnerList.searchPartnerId"
+        styleSet={styles}
+        id="partner_organisation_filter"
+      />
+      <TextInputComponent
+        fieldName="policyGroup"
+        onTextChange={onFilterChangeEvent}
+        fieldNameKey="partnerList.policyGroup"
+        placeHolderKey="partnerList.searchPolicyGroup"
+        styleSet={styles}
+        id="policy_group_filter"
+      />
+      <TextInputComponent
+        fieldName="emailID"
+        onTextChange={onFilterChangeEvent}
+        fieldNameKey="partnerList.email"
+        placeHolderKey="partnerList.searchEmailAddress"
+        styleSet={styles}
+        id="email_address_filter"
+      />
+      <DropdownComponent
+        fieldName="certUploadStatus"
+        dropdownDataList={certUploadStatus}
+        onDropDownChangeEvent={onFilterChangeEvent}
+        fieldNameKey="partnerList.certUploadStatus"
+        placeHolderKey="partnerList.selectCertUploadStatus"
+        styleSet={styles}
+        isPlaceHolderPresent={true}
+        id="cert_upload_status_filter"
+      />
+      <DropdownComponent
+        fieldName="status"
+        dropdownDataList={status}
+        onDropDownChangeEvent={onFilterChangeEvent}
+        fieldNameKey="partnerList.status"
+        placeHolderKey="partnerList.selectStatus"
+        styleSet={styles}
+        isPlaceHolderPresent={true}
+        id="status_filter"
+      />
+      <div className="ml-auto mt-6 mr-6">
+        <button
+          id="apply_filter__btn"
+          onClick={() => onFilterChange(filters)}
+          type="button"
+          className="h-10 text-sm font-semibold px-7 text-white bg-tory-blue rounded-md ml-6"
+        >
+          {t("partnerList.applyFilter")}
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
