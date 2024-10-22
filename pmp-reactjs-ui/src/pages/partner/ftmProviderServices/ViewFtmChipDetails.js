0560e1c786fd64c0c47db913db101aadf41a0ec2
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-10-22 13:29:04 +0530

    MOSIP-36321, MOSIP-36331: Refactored the get certificate functions.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/partner/ftmProviderServices/ViewFtmChipDetails.js
+++ pmp-reactjs-ui/src/pages/partner/ftmProviderServices/ViewFtmChipDetails.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { getUserProfile } from "../../../services/UserProfileService";
-import { bgOfStatus, formatDate, getStatusCode, isLangRTL, getPartnerDomainType, getPartnerManagerUrl } from "../../../utils/AppUtils";
+import { bgOfStatus, formatDate, getStatusCode, isLangRTL, getPartnerDomainType, getPartnerManagerUrl, downloadCertificate } from "../../../utils/AppUtils";
 import Title from "../../common/Title";
 import fileUploadBlue from '../../../svg/file_upload_blue_icon.svg';
 import fileUploadDisabled from '../../../svg/file_upload_disabled_icon.svg';
@@ -79,21 +79,6 @@ function ViewFtmChipDetails() {
                 downloadCertificate(response.caSignedCertificateData, 'ca_signed_ftm_certificate.cer')
             }
         }
-    }
-
-    const downloadCertificate = (certificateData, fileName) => {
-        const blob = new Blob([certificateData], { type: 'application/x-x509-ca-cert' });
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = fileName;
-
-        document.body.appendChild(link);
-        link.click();
-
-        // Cleanup
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(link);
     }
 
     const getCertificate = async (ftmId) => {
