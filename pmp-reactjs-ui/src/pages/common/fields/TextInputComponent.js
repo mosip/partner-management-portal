e2401110c3a8efe4ccc2830277c472769aada27d
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-10-28 11:53:04 +0530

    MOSIP-36490: Enhanced the clearing text cross-icon in Input tab of Filter part.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/common/fields/TextInputComponent.js
+++ pmp-reactjs-ui/src/pages/common/fields/TextInputComponent.js
@@ -15,6 +15,12 @@ function TextInputComponent({ fieldName, fieldNameKey, placeHolderKey, onTextCha
         onTextChange(fieldName, newValue);
     };
 
+    const onTextClear = (event) => {
+        const newValue = event.target.value;
+        setInputValue('');
+        onTextChange(fieldName, newValue);
+    };
+
     return (
         <div className={`ml-4 mb-2 ${(styleSet && styleSet.outerDiv) ? styleSet.outerDiv : ''}`}>
             <label className={`flex items-center text-dark-blue text-sm mb-2 ${(styleSet && styleSet.inputLabel) ? styleSet.inputLabel : ''}`}>
@@ -23,7 +29,7 @@ function TextInputComponent({ fieldName, fieldNameKey, placeHolderKey, onTextCha
             <div className={`flex border border-[#707070] rounded-[4px] text-[15px] items-center`}>
                 <input id={id} type="text" value={inputValue} onChange={handleInputChange} placeholder={t(placeHolderKey)} className={`rounded-md h-[2.1rem] ${isLoginLanguageRTL ? 'pr-3' : 'pl-3'} focus:outline-none items-center ${styleSet && styleSet.inputField ? styleSet.inputField : ''}`} />
                 {inputValue &&
-                    (<p onClick={() => setInputValue('')} className={`bg-white font-bold rounded-md px-2 ${isLoginLanguageRTL ? '-mr-6' : '-ml-6'} focus:outline-none items-center hover:cursor-pointer`}>
+                    (<p onClick={onTextClear} className={`bg-white font-bold rounded-md px-2 ${isLoginLanguageRTL ? '-mr-6' : '-ml-6'} focus:outline-none items-center hover:cursor-pointer`}>
                         x
                     </p>
                     )}
