d6acd286c9770e8536a27f1bb446094003dc3710
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-11-06 16:16:18 +0530

    MOSIP-36981, MOSIP-36991: As per policy-type titling the download JSON file name in local. (#897)
    * MOSIP-36981, MOSIP-36991: Implemented Policy JSON preview in Auth & Data Share View Page.
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * MOSIP-36981, MOSIP-36991: Implemented Policy JSON preview in Auth & Data Share View Page.
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * MOSIP-36981, MOSIP-36991: Implemented Policy JSON preview in Auth & Data Share View Page.
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/admin/policyManager/ViewPolicyPopup.js
+++ pmp-reactjs-ui/src/pages/admin/policyManager/ViewPolicyPopup.js
@@ -4,11 +4,11 @@ import { useNavigate } from 'react-router-dom';
 
 function ViewPolicyPopup({ title, downloadJsonFile, closePopUp, jsonData }) {
     const { t } = useTranslation();
-    const previewData = JSON.stringify(jsonData)
+    const previewData = JSON.stringify(jsonData, null, 2);
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-[50%] z-50 font-inter cursor-default">
-            <div className='flex-col bg-white w-[45rem] min-h-[6rem] rounded-md relative top-[2.5rem]'>
+            <div className='flex-col bg-white w-[45rem] max-h-[33rem] rounded-md relative'>
                 <div className='flex py-2 px-5 justify-between items-center'>
                     <h2 className='font-semibold text-sm'>{title}</h2>
                     <div className='flex gap-x-9 items-center'>
@@ -20,7 +20,7 @@ function ViewPolicyPopup({ title, downloadJsonFile, closePopUp, jsonData }) {
                 </div>
                 <hr className={`h-px w-full bg-gray-200 border-0`} />
                 <div className='mx-6 my-2 max-h-[27rem] overflow-y-scroll'>
-                    <pre>{JSON.stringify(jsonData, undefined, 2)}</pre>
+                    <pre>{previewData}</pre>
                 </div>
             </div>
         </div>
