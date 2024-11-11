3b250a096771e102a3d1dc3725981cd3c6d05195
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2024-11-11 12:32:03 +0530

    MOSIP-36598, MOSIP-36971, MOSIP-36981, MOSIP-36991: Resolved the issues. (#913)
    * MOSIP-36598, MOSIP-36971, MOSIP-36981, MOSIP-36991: Resolved the issues
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * MOSIP-36598, MOSIP-36971, MOSIP-36981, MOSIP-36991: Resolved the issues
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * MOSIP-36598, MOSIP-36971, MOSIP-36981, MOSIP-36991: Resolved the issues
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    * MOSIP-36598, MOSIP-36971, MOSIP-36981, MOSIP-36991: Resolved the issues
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-reactjs-ui/src/pages/admin/policyManager/ViewPolicyGroupDetails.js
+++ pmp-reactjs-ui/src/pages/admin/policyManager/ViewPolicyGroupDetails.js
@@ -28,78 +28,80 @@ function ViewPolicyGroupDetails() {
     }, []);
 
     return (
-        <div className={`mt-2 w-[100%] ${isLoginLanguageRTL ? "mr-28 ml-5" : "ml-28 mr-5"} font-inter`}>
-            <div className="flex justify-between mb-3">
-                <Title title='viewPolicyGroupDetails.viewPolicyGroup' subTitle='viewPolicyGroupDetails.listOfPolicyGroups' backLink='/partnermanagement/admin/policy-manager/policy-group-list' />
-            </div>
-            {unexpectedError && (
-                <div className={`bg-[#FCFCFC] w-full mt-3 rounded-lg shadow-lg items-center`}>
-                    <div className="flex items-center justify-center p-24">
-                        <div className="flex flex-col justify-center items-center">
-                            <img className="max-w-60 min-w-52 my-2" src={somethingWentWrongIcon} alt="" />
-                            <p className="text-sm font-semibold text-[#6F6E6E] py-4">{t('devicesList.unexpectedError')}</p>
-                            <button onClick={moveToPolicyGroupList} type="button"
-                                className={`w-32 h-10 flex items-center justify-center font-semibold rounded-md text-sm mx-8 py-3 bg-tory-blue text-white`}>
-                                {t('commons.goBack')}
+        <>
+            <div className={`w-[100%] ${isLoginLanguageRTL ? "mr-28 ml-5" : "ml-28 mr-5"} font-inter mt-7 overflow-x-scroll`}>
+                <div className="flex justify-between mb-3">
+                    <Title title='viewPolicyGroupDetails.viewPolicyGroup' subTitle='viewPolicyGroupDetails.listOfPolicyGroups' backLink='/partnermanagement/admin/policy-manager/policy-group-list' />
+                </div>
+                {unexpectedError && (
+                    <div className={`bg-[#FCFCFC] w-full mt-3 rounded-lg shadow-lg items-center`}>
+                        <div className="flex items-center justify-center p-24">
+                            <div className="flex flex-col justify-center items-center">
+                                <img className="max-w-60 min-w-52 my-2" src={somethingWentWrongIcon} alt="" />
+                                <p className="text-sm font-semibold text-[#6F6E6E] py-4">{t('devicesList.unexpectedError')}</p>
+                                <button onClick={moveToPolicyGroupList} type="button"
+                                    className={`w-32 h-10 flex items-center justify-center font-semibold rounded-md text-sm mx-8 py-3 bg-tory-blue text-white`}>
+                                    {t('commons.goBack')}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+                {!unexpectedError && (
+                    <div className="bg-snow-white h-fit mt-1 rounded-t-xl shadow-lg font-inter">
+                        <div className="flex justify-between px-7 pt-3 border-b max-[450px]:flex-col">
+                            <div className="flex-col">
+                                <p className="font-semibold text-lg text-dark-blue mb-2">
+                                    {policyGroupDetails.name}
+                                </p>
+                                <div className="flex items-center justify-start mb-2 max-[400px]:flex-col max-[400px]:items-start">
+                                    <div className={`${bgOfStatus(policyGroupDetails.isActive ? 'ACTIVE' : 'INACTIVE', t)} flex w-fit py-1 px-5 text-xs rounded-md my-2 font-semibold`}>
+                                        {getStatusCode(policyGroupDetails.isActive ? 'active' : 'inactive', t)}
+                                    </div>
+                                    <div className={`font-semibold ${isLoginLanguageRTL ? "mr-1" : "ml-3"} text-sm text-dark-blue`}>
+                                        {t("viewDeviceDetails.createdOn") + ' ' +
+                                            formatDate(policyGroupDetails.crDtimes, "date", true)
+                                        }
+                                    </div>
+                                    <div className="mx-1 text-gray-300">|</div>
+                                    <div className="font-semibold text-sm text-dark-blue">
+                                        {formatDate(policyGroupDetails.crDtimes, "time", true)}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${isLoginLanguageRTL ? "pr-8 ml-8" : "pl-8 mr-8"} pt-3 mb-3`}>
+                            <div className="flex flex-wrap py-3 max-[450px]:flex-col">
+                                <div className="w-[50%] max-[600px]:w-[100%] mb-3">
+                                    <p className="font-[600] text-suva-gray text-sm">
+                                        {t("viewPolicyGroupDetails.policyGroupId")}
+                                    </p>
+                                    <p className="font-[600] text-vulcan text-md">
+                                        {policyGroupDetails.id}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap py-3 max-[450px]:flex-col">
+                                <div className="w-full mb-5">
+                                    <p className="font-[600] text-suva-gray text-sm">
+                                        {t("viewPolicyGroupDetails.policyGroupDesc")}
+                                    </p>
+                                    <p className="font-[600] text-vulcan text-md">
+                                        {policyGroupDetails.desc}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <hr className={`h-[0.1rem] w-[95%] bg-gray-200 border-0 ${isLoginLanguageRTL ? "mr-6" : "ml-6"}`} />
+                        <div className={`flex justify-end py-8 ${isLoginLanguageRTL ? "ml-8" : "mr-8"}`}>
+                            <button id="policy_group_view_back_btn" onClick={moveToPolicyGroupList} className={`h-11 w-[9rem] text-sm text-tory-blue bg-white border border-blue-800 font-semibold rounded-md text-center`}>
+                                {t("commons.goBack")}
                             </button>
                         </div>
                     </div>
-                </div>
-            )}
-            {!unexpectedError && (
-                <div className="bg-snow-white h-fit mt-1 rounded-t-xl shadow-lg font-inter">
-                    <div className="flex justify-between px-7 pt-3 border-b max-[450px]:flex-col">
-                        <div className="flex-col">
-                            <p className="font-semibold text-lg text-dark-blue mb-2">
-                                {policyGroupDetails.name}
-                            </p>
-                            <div className="flex items-center justify-start mb-2 max-[400px]:flex-col max-[400px]:items-start">
-                                <div className={`${bgOfStatus(policyGroupDetails.isActive ? 'ACTIVE' : 'INACTIVE', t)} flex w-fit py-1 px-5 text-xs rounded-md my-2 font-semibold`}>
-                                    {getStatusCode(policyGroupDetails.isActive ? 'active' : 'inactive', t)}
-                                </div>
-                                <div className={`font-semibold ${isLoginLanguageRTL ? "mr-1" : "ml-3"} text-sm text-dark-blue`}>
-                                    {t("viewDeviceDetails.createdOn") + ' ' +
-                                        formatDate(policyGroupDetails.crDtimes, "date", true)  
-                                    }
-                                </div>
-                                <div className="mx-1 text-gray-300">|</div>
-                                <div className="font-semibold text-sm text-dark-blue">
-                                    {formatDate(policyGroupDetails.crDtimes, "time", true)}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${isLoginLanguageRTL ? "pr-8 ml-8" : "pl-8 mr-8"} pt-3 mb-3`}>
-                        <div className="flex flex-wrap py-3 max-[450px]:flex-col">
-                            <div className="w-[50%] max-[600px]:w-[100%] mb-3">
-                                <p className="font-[600] text-suva-gray text-sm">
-                                    {t("viewPolicyGroupDetails.policyGroupId")}
-                                </p>
-                                <p className="font-[600] text-vulcan text-md">
-                                    {policyGroupDetails.id}
-                                </p>
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap py-3 max-[450px]:flex-col">
-                            <div className="w-full mb-5">
-                                <p className="font-[600] text-suva-gray text-sm">
-                                    {t("viewPolicyGroupDetails.policyGroupDesc")}
-                                </p>
-                                <p className="font-[600] text-vulcan text-md">
-                                  {policyGroupDetails.desc}
-                                  </p>
-                            </div>
-                        </div>
-                    </div>
-                    <hr className={`h-[0.1rem] w-[95%] bg-gray-200 border-0 ${isLoginLanguageRTL ? "mr-6" : "ml-6"}`} />
-                    <div className={`flex justify-end py-8 ${isLoginLanguageRTL ? "ml-8" : "mr-8"}`}>
-                        <button id="policy_group_view_back_btn" onClick={moveToPolicyGroupList} className={`h-11 w-[9rem] text-sm text-tory-blue bg-white border border-blue-800 font-semibold rounded-md text-center`}>
-                            {t("commons.goBack")}
-                        </button>
-                    </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
     )
 }
 
