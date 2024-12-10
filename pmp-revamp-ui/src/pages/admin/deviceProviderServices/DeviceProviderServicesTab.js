6e92e7bf706f6370800954b6e1b92483159b831c
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-12-10 12:43:28 +0530

    MOSIP-37719 - Admin Devices List page. (#1009)
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-revamp-ui/src/pages/admin/deviceProviderServices/DeviceProviderServicesTab.js
+++ pmp-revamp-ui/src/pages/admin/deviceProviderServices/DeviceProviderServicesTab.js
@@ -21,7 +21,7 @@ function DeviceProviderServiceTab ( {activeSbi, sbiListPath, activeDevice, devic
         <div className='flex text-xs bg-[#FCFCFC] font-bold space-x-16 items-start rounded-lg px-[1.5%] pt-[2%]'>
             <div className={`flex-col justify-center`}>
                 <h6 id='sbi_tab' onClick={changeToSbi}
-                    className={`${activeSbi ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] ${isLoginLanguageRTL ? "mr-[10%]" : "ml-[10%]"} cursor-pointer text-sm`}
+                    className={`${activeSbi ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] cursor-pointer text-sm text-center`}
                     tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, changeToSbi)}>
                     {t('deviceProviderServices.sbiDetails')}
                 </h6>
@@ -29,7 +29,7 @@ function DeviceProviderServiceTab ( {activeSbi, sbiListPath, activeDevice, devic
             </div>
             <div className={`flex-col justify-center`}>
                 <h6 id='devices_tab' onClick={changeToDevice}
-                    className={`${activeDevice ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] ${isLoginLanguageRTL ? "mr-[10%]" : "ml-[10%]"} cursor-pointer text-sm`}
+                    className={`${activeDevice ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] cursor-pointer text-sm text-center`}
                     tabIndex="0" onKeyPress={(e) => onPressEnterKey(e, changeToDevice)}>
                     {t('deviceProviderServices.deviceDetails')}
                 </h6>
