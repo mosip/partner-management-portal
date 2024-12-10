d36f43d0bd644865977cabda612361850fdd680a
Santhosh <102220709+santhoshsunder@users.noreply.github.com>
2024-12-10 19:53:18 +0530

    Update CreateOidecClient.java
    Signed-off-by: Santhosh <102220709+santhoshsunder@users.noreply.github.com>

--- uitest-pmprevamp/src/main/java/io/mosip/testrig/pmprevampui/testcase/CreateOidecClient.java
+++ uitest-pmprevamp/src/main/java/io/mosip/testrig/pmprevampui/testcase/CreateOidecClient.java
@@ -174,44 +174,12 @@ public class CreateOidecClient extends BaseClass {
 	  
 	  }
 	
-	@Test
-	public void CreateOidecClientcWithSpaceValu() {
-
-		DashboardPage dashboardpage = new DashboardPage(driver);
-
-		assertTrue(dashboardpage.isAuthenticationServicesTitleDisplayed(), GlobalConstants.isAuthenticationServicesDisplayed);
-		OidcClientPage oidcClientPage=dashboardpage.clickOnAuthenticationServicesTitle();
-
-		assertTrue(oidcClientPage.isCreateOidcClientDisplayed(), GlobalConstants.isCreateOIDCClientDisplayed);
-		oidcClientPage.clickOnCreateOidcClientButton();
-	
-		assertTrue(oidcClientPage.isPartnerIdDropdownDisplayed(), GlobalConstants.isPartnerIdDropdownDisplayed);
-		oidcClientPage.selectPartnerIdDropdown();
-		
-		assertTrue(oidcClientPage.isPolicyNameDropdownDisplayed(), GlobalConstants.isPolicyNameDropdownDisplayed);
-		oidcClientPage.selectPolicyNameDropdown(data+"ad");
-		
-		oidcClientPage.enterNameOidcTextBox(" ");
-		
-		oidcClientPage.enterPublicKeyTextBox(" ");
-		oidcClientPage.enterLogoUrTextBox(" ");
-		oidcClientPage.enterRedirectUriTextBox(" ");
-		
-		assertTrue(oidcClientPage.isEnterValidUriForLogoUriTextDisplayed(), GlobalConstants.isEnterValidLogoUriTextDisplayed);
-		assertTrue(oidcClientPage.isEnterValidUriForRedirectUriTextDisplayed(), GlobalConstants.isEnterRedirectUriTextDisplayed);
-		
-	}
-	
-	@Test
-	public void CreateOidecClientWithouUploadingCertificates() {
-
 	  @Test public void ClearFormOidecClient() {
 		  DashboardPage dashboardpage = new DashboardPage(driver);
 	  RegisterPage registerPage = new
 	  RegisterPage(driver);
-	  assertTrue(dashboardpage.isAuthenticationServicesTitleDisplayed(),
-	  GlobalConstants.isAuthenticationServicesDisplayed); OidcClientPage 
-	  oidcClientPage =dashboardpage.clickOnAuthenticationServicesTitle(); 
+	  assertTrue(dashboardpage.isAuthenticationServicesTitleDisplayed(), GlobalConstants.isAuthenticationServicesDisplayed); 
+	  OidcClientPage oidcClientPage =dashboardpage.clickOnAuthenticationServicesTitle(); 
 	  assertTrue(oidcClientPage.isCreateOidcClientDisplayed(),
 	  GlobalConstants.isCreateOIDCClientDisplayed); 
 	  oidcClientPage.clickOnCreateOidcClientButton(); 
@@ -265,51 +233,4 @@ public class CreateOidecClient extends BaseClass {
 		assertTrue(oidcClientPage.isInvalidLogoUriErrorVissible(), GlobalConstants.isInvalidLogoUriErrorVissible); 
 		
 	 }
-
-		ApiKeyPage apiKeyPage = new ApiKeyPage(driver);
-		dashboardpage.clickOnProfileDropdown();
-		assertTrue(dashboardpage.isLogoutButtonDisplayed(), GlobalConstants.isLogoutButtonDisplayed);
-
-		LoginPage loginpage = dashboardpage.clickOnLogoutButton();
-		
-		loginpage.enterUserName("0"+ data+"n");
-		loginpage.enterPassword(password);
-		loginpage.ClickOnLoginButton();
-
-		assertTrue(dashboardpage.isAuthenticationServicesTitleDisplayed(), GlobalConstants.isAuthenticationServicesDisplayed);
-		OidcClientPage oidcClientPage=dashboardpage.clickOnAuthenticationServicesTitle();
-		
-		assertTrue(oidcClientPage.isCreateOidcClientDisplayed(), GlobalConstants.isCreateOIDCClientDisplayed);		
-		assertTrue(oidcClientPage.isPolicyGroupHeaderTextDisplayed(), GlobalConstants.isPolicyGroupHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isPolicyNameHeaderTextDisplayed(), GlobalConstants.isPolicyNameHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isOIDCClientNameHeaderTextDisplayed(), GlobalConstants.isOIDCClientNameHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isCreatedDateHeaderTextDisplayed(), GlobalConstants.isCreatedDateHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isStatusHeaderTextDisplayed(), GlobalConstants.isStatusHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isOIDCClientIDHeaderTextDisplayed(), GlobalConstants.isOIDCClientIDHeaderTextDisplayed);
-		assertTrue(oidcClientPage.isActionHeaderTextDisplayed(), GlobalConstants.isActionHeaderTextDisplayed);
-		oidcClientPage.clickOnCreateOidcClientButton();
-	
-		assertTrue(oidcClientPage.isPartnerIdDropdownDisplayed(), GlobalConstants.isPartnerIdDropdownDisplayed);
-//		assertTrue(oidcClientPage.isUserIdDoesNotExistsPopupDisplayed(), GlobalConstants.isPartnerIdDropdownDisplayed);
-		oidcClientPage.clickOnPartnerIdDropdown();
-		
-		assertTrue(oidcClientPage.isNoDataAvailableTextDisplayed(), GlobalConstants.isNoDataAvailableTextDisplayed);
-		
-		String publicKey = KeycloakUserManager.readJsonData(TestRunner.getResourcePath() + "/" + "config/"+"/publicKey.json").toString();
-		oidcClientPage.enterPublicKeyTextBox(publicKey);
-		oidcClientPage.enterLogoUrTextBox(ConfigManager.getLogouri());
-		oidcClientPage.enterRedirectUriTextBox(ConfigManager.getRedirectUri());
-		oidcClientPage.clickOnAddNewRedirectUrlButton();
-		oidcClientPage.EntercreateOidcRedirectUrl2(ConfigManager.getRedirectUri()+"a");
-		oidcClientPage.clickOnAddNewRedirectUrlButton();
-		oidcClientPage.EntercreateOidcRedirectUrl3(ConfigManager.getRedirectUri()+"b");
-		oidcClientPage.clickOnAddNewRedirectUrlButton();
-		oidcClientPage.EntercreateOidcRedirectUrl4(ConfigManager.getRedirectUri()+"c");
-		oidcClientPage.clickOnAddNewRedirectUrlButton();
-		oidcClientPage.EntercreateOidcRedirectUrl5(ConfigManager.getRedirectUri()+"d");
-		
-		oidcClientPage.clickOnCreateOidcClearForm();
-		
-		assertFalse(oidcClientPage.isCreateOidcRedirectUrl5Displayed(), GlobalConstants.isNoDataAvailableTextDisplayed);
-	}
 }
