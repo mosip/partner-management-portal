2fd001785d70e222ffa0c0548fbb5481cdaf756d
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2024-12-12 16:38:15 +0530

    MOSIP-36805 : Partners' details (Partner Admin) : First Name and Last Name of Partner to be displayed in View individual Partner detail (#1018)
    * MOSIP-36805 : Partners' details (Partner Admin) : First Name and Last Name of Partner to be displayed in View individual Partner detail
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    * MOSIP-36805 : Partners' details (Partner Admin) : First Name and Last Name of Partner to be displayed in View individual Partner detail
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>
    
    ---------
    
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-revamp-ui/src/pages/admin/partners/ViewPartnerDetails.js
+++ pmp-revamp-ui/src/pages/admin/partners/ViewPartnerDetails.js
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { getUserProfile } from '../../../services/UserProfileService';
 import { downloadFile, formatDate, getPartnerManagerUrl, getCertificate,
-     handleMouseClickForDropdown, handleServiceErrors, isLangRTL, getErrorMessage } from '../../../utils/AppUtils';
+     handleMouseClickForDropdown, handleServiceErrors, isLangRTL, getErrorMessage, getPartnerTypeDescription } from '../../../utils/AppUtils';
 import SuccessMessage from '../../common/SuccessMessage';
 import Title from '../../common/Title';
 
@@ -182,7 +182,7 @@ function ViewPartnerDetails() {
                                                 {t("viewOidcClientDetails.partnerType")}
                                             </p>
                                             <p className="font-[600] text-vulcan text-sm">
-                                                {t(partnerDetails.partnerType)}
+                                                {getPartnerTypeDescription(partnerDetails.partnerType, t)}
                                             </p>
                                         </div>
                                         <div className="w-[50%] max-[600px]:w-[100%] mb-3">
@@ -193,12 +193,28 @@ function ViewPartnerDetails() {
                                                 {partnerDetails.organizationName}
                                             </p>
                                         </div>
+                                        <div className="w-[50%] max-[600px]:w-[100%] mb-3">
+                                            <p className="font-[600] text-suva-gray text-xs">
+                                                {t("viewPartnerDetails.firstName")}
+                                            </p>
+                                            <p className="font-[600] text-vulcan text-sm">
+                                                {partnerDetails.firstName ?? "-"}
+                                            </p>
+                                        </div>
+                                        <div className="w-[50%] max-[600px]:w-[100%] mb-3">
+                                            <p className="font-[600] text-suva-gray text-xs">
+                                                {t("viewPartnerDetails.lastName")}
+                                            </p>
+                                            <p className="font-[600] text-vulcan text-sm">
+                                                {partnerDetails.lastName ?? "-"}
+                                            </p>
+                                        </div>
                                         <div className="mb-3 max-[600px]:w-[100%] w-[50%]">
                                             <p className="font-[600] text-suva-gray text-xs">
                                                 {t("userProfile.phoneNumber")}
                                             </p>
                                             <p className="font-[600] text-vulcan text-sm">
-                                                {t(partnerDetails.contactNumber ? partnerDetails.contactNumber : '-')}
+                                                {partnerDetails.contactNumber ? partnerDetails.contactNumber : '-'}
                                             </p>
                                         </div>
                                         <div className="mb-3 max-[600px]:w-[100%] w-[50%]">
@@ -206,7 +222,7 @@ function ViewPartnerDetails() {
                                                 {t("userProfile.emailAddress")}
                                             </p>
                                             <p className="font-[600] text-vulcan text-sm">
-                                                {t(partnerDetails.emailId)}
+                                                {partnerDetails.emailId}
                                             </p>
                                         </div>
                                         <div className="mb-3 w-[100%]">
@@ -214,7 +230,7 @@ function ViewPartnerDetails() {
                                                 {t("viewOidcClientDetails.policyGroup")}
                                             </p>
                                             <p className="font-[600] text-vulcan text-sm">
-                                                {t(partnerDetails.policyGroupName ? partnerDetails.policyGroupName : '-')}
+                                                {partnerDetails.policyGroupName ? partnerDetails.policyGroupName : '-'}
                                             </p>
                                         </div>
                                     </div>
