e660a7e2437fe53504d5d419b769cb4db8b0bd84
Madhuravas reddy <madhu@mosip.io>
2024-12-17 17:00:26 +0530

    MOSIP-38322 MOSIP-38329 MOSIP-38345 MOSIP-38328 resolved minor bugs (#1035)
    * MOSIP-38322 added question mark in Approve/Reject policy popup
    
    Signed-off-by: MadhuMosip <madhu@mosip.io>
    
    * MOSIP-38329 Added X icon to close Clone Policy popup
    
    Signed-off-by: MadhuMosip <madhu@mosip.io>
    
    * MOSIP-38345 removed curser pointing in clone policy popup
    
    Signed-off-by: MadhuMosip <madhu@mosip.io>
    
    * MOSIP-38328 added double quotes for policyName and policyGroupName in clone policy success message
    
    Signed-off-by: MadhuMosip <madhu@mosip.io>
    
    ---------
    
    Signed-off-by: MadhuMosip <madhu@mosip.io>

--- pmp-revamp-ui/src/pages/admin/policyManager/ClonePolicyPopup.js
+++ pmp-revamp-ui/src/pages/admin/policyManager/ClonePolicyPopup.js
@@ -7,6 +7,7 @@ import FocusTrap from 'focus-trap-react';
 import { HttpService } from '../../../services/HttpService.js';
 import { getPolicyGroupList, getPolicyManagerUrl, createRequest, getPolicyDetails } from '../../../utils/AppUtils.js';
 import SuccessMessage from '../../common/SuccessMessage.js';
+import closeIcon from "../../../svg/close_icon.svg";
 
 function ClonePolicyPopup ({policyDetails, closePopUp}) {
     const [selectedPolicyGroup, setSelectedPolicyGroup] = useState('');
@@ -120,7 +121,7 @@ function ClonePolicyPopup ({policyDetails, closePopUp}) {
     }
 
     return (
-        <div className="fixed inset-0 w-full flex items-center justify-center bg-black bg-opacity-50 z-50 font-inter">
+        <div className="fixed inset-0 w-full flex items-center justify-center bg-black bg-opacity-50 z-50 font-inter cursor-default">
             <FocusTrap focusTrapOptions={{ initialFocus: false, allowOutsideClick: true }}>
                 <div className={`bg-white md:w-[25rem] w-[60%] h-fit rounded-xl shadow-lg`}>
                     {!dataLoaded && (
@@ -128,9 +129,10 @@ function ClonePolicyPopup ({policyDetails, closePopUp}) {
                     )}
                     {dataLoaded && (
                         <div className="relative">
-                            <div className="px-6 py-3">
+                            <div className="px-6 py-3 flex justify-between">
                                 <h3 className="text-lg font-bold text-[#333333]">{t('clonePolicyPopup.title')}</h3>
-                            </div>
+                                <button onClick={cancelPopUp}><img className='h-[25px] w-[25px]' src={closeIcon} alt='closeIcon'/></button>
+                            </div> 
                             <div className="border-gray-200 border-opacity-75 border-t"></div>
                             {errorMsg && (
                                 <ErrorMessage errorCode={errorCode} errorMessage={errorMsg} clickOnCancel={cancelErrorMsg} customStyle={customStyle}/>
