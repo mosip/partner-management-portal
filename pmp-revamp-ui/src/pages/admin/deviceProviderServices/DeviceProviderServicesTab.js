1ffa82350dace7de3009bea0ee3c9a850ffa4293
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-01-08 07:04:44 +0530

    MOSIP-36036: Sonar bugs, security hotspots, duplications for UI code
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-revamp-ui/src/pages/admin/deviceProviderServices/DeviceProviderServicesTab.js
+++ pmp-revamp-ui/src/pages/admin/deviceProviderServices/DeviceProviderServicesTab.js
@@ -18,16 +18,18 @@ function DeviceProviderServiceTab({ activeSbi, sbiListPath, activeDevice, device
     return (
         <div className='flex text-xs bg-[#FCFCFC] font-bold space-x-16 items-start rounded-lg px-[1.5%] pt-[2%]'>
             <div className={`flex-col items-center`}>
-                <button id='sbi_tab' onClick={changeToSbi} className={`${activeSbi ? "text-[#1447b2]" : "text-[#031640]"} mb-[0.6rem] cursor-pointer text-sm`}>
+                <div role='button' id='sbi_tab' onClick={changeToSbi} className={`${activeSbi ? "text-[#1447b2]" : "text-[#031640]"} mb-[0.6rem] cursor-pointer text-sm text-center`}
+                    tabIndex="0" onKeyDown={(e) => onPressEnterKey(e, changeToSbi)}>
                     <h6> {t('deviceProviderServices.sbiDetails')} </h6>
-                </button>
+                </div>
 
                 <div className={`h-1 w-20 ${activeSbi ? "bg-tory-blue" : "bg-transparent"}  rounded-t-md`}></div>
             </div>
             <div className={`flex-col justify-center`}>
-                <button id='devices_tab' onClick={changeToDevice} className={`${activeDevice ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] cursor-pointer text-sm text-center`}>
+                <div role='button' id='devices_tab' onClick={changeToDevice} className={`${activeDevice ? "text-[#1447b2]" : "text-[#031640]"} mb-[12%] cursor-pointer text-sm text-center`}
+                    tabIndex="0" onKeyDown={(e) => onPressEnterKey(e, changeToDevice)}>
                     <h6> {t('deviceProviderServices.deviceDetails')} </h6>
-                </button>
+                </div>
 
                 <div className={`h-1 w-20 ${activeDevice ? "bg-tory-blue" : "bg-transparent"} rounded-t-md`}></div>
             </div>
