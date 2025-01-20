2c95d84603dbda87b27ae624a8db719dad49421b
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2025-01-20 18:36:40 +0530

    MOSIP-34977: Updated cert upload and expiry date time (#1118)
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    Co-authored-by: Swetha K <swetha.k@technoforte.co.in>

--- pmp-revamp-ui/src/pages/admin/ftmProviderServices/ViewAdminFtmChipDetails.js
+++ pmp-revamp-ui/src/pages/admin/ftmProviderServices/ViewAdminFtmChipDetails.js
@@ -233,13 +233,13 @@ function ViewAdminFtmChipDetails() {
                                                 <div className={`flex-col ${isLoginLanguageRTL ? "mr-[10%]" : "ml-[10%]"} space-y-1`}>
                                                     <p id="ftm_chip_details_label_upload_date_time" className="font-semibold text-sm text-dim-gray">{t('partnerCertificatesList.timeOfUpload')}</p>
                                                     <p id="ftm_chip_details_context_upload_date_time" className="font-semibold text-md text-charcoal-gray">
-                                                        {formatDate(certificateDetails.caSignedCertUploadDateTime, 'dateTime', false)}
+                                                        {formatDate(certificateDetails.mosipSignedCertUploadDateTime, 'dateTime', true)}
                                                     </p>
                                                 </div>
                                                 <div className={`flex-col ${isLoginLanguageRTL ? "mr-[5%]" : "ml-[5%]"} space-y-1`}>
                                                     <p id="ftm_chip_details_label_expiry_date_time" className={`text-sm font-semibold text-dim-gray font-semibold'}`}>{t('partnerCertificatesList.expiryDate')}</p>
                                                     <p id="ftm_chip_details_context_expiry_date_time" className={`text-md ${certificateDetails.isCaSignedCertificateExpired ? 'text-crimson-red font-bold' : 'text-charcoal-gray font-semibold'}`}>
-                                                        {formatDate(certificateDetails.caSignedCertExpiryDateTime, 'dateTime', false)}
+                                                        {formatDate(certificateDetails.caSignedCertExpiryDateTime, 'dateTime', true)}
                                                     </p>
                                                 </div>
                                             </div>
