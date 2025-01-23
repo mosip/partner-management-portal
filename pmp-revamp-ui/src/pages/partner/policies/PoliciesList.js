8dabf5387a4b10443163d534da2294e245f83612
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-01-23 16:30:32 +0530

    MOSIP-38274, MOSIP-39144, MOSIP-39146 : Resolved the bugs.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-revamp-ui/src/pages/partner/policies/PoliciesList.js
+++ pmp-revamp-ui/src/pages/partner/policies/PoliciesList.js
@@ -228,7 +228,7 @@ function PoliciesList() {
                                     {
                                       viewPolicyId === index && (
                                         <div role='button' id='policy_list_view_card' onClick={() => showViewPolicyDetails(partner)}
-                                          className={`flex justify-between border bg-white absolute text-xs font-semibold rounded-md shadow-md w-[5.5rem] px-2 py-2 z-20 items-center cursor-pointer ${isLoginLanguageRTL ? "left-[4.5rem] text-right" : "right-[4.5rem] text-left"}`}>
+                                          className={`flex justify-between border bg-white absolute text-xs font-semibold rounded-md shadow-md w-[6rem] px-2 py-2 z-20 items-center cursor-pointer ${isLoginLanguageRTL ? "left-[4.5rem] text-right" : "right-[4.5rem] text-left"}`}>
                                           <p> {t('policies.view')} </p>
                                           <img src={viewIcon} alt="" className={`${isLoginLanguageRTL ? "pl-2" : "pr-2"}`} />
                                         </div>
