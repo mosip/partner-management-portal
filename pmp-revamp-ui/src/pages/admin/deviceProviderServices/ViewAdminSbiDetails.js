802d8accd579eb3a8e6f65cd2fb539eb18b1f67d
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-01-29 11:20:34 +0530

    MOSIP-38879: Resolved the alignment issue in details page.
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-revamp-ui/src/pages/admin/deviceProviderServices/ViewAdminSbiDetails.js
+++ pmp-revamp-ui/src/pages/admin/deviceProviderServices/ViewAdminSbiDetails.js
@@ -34,7 +34,7 @@ function ViewAdminSbiDetails() {
     };
 
     return (
-        <div className={`mt-2 w-[100%] ${isLoginLanguageRTL ? "mr-28 ml-5" : "ml-28 mr-5"} font-inter relative`}>
+        <div className={`flex-col w-full p-4 bg-anti-flash-white h-full font-inter break-words max-[450px]:text-sm mb-[2%] ${isLoginLanguageRTL ? "mr-24 ml-1" : "ml-24 mr-1"} overflow-x-scroll`}>
             <div className={`flex-col mt-4 bg-anti-flash-white h-full font-inter break-words max-[450px]:text-sm mb-[2%]`}>
                 <div className="flex justify-between mb-3">
                     <Title title={'viewSbiDetails.viewSbiDetails'} subTitle='sbiList.listOfSbis' backLink='/partnermanagement/admin/device-provider-services/sbi-list' />
