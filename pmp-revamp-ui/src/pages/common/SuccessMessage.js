695b7e966111fcf917f619723300d7731714ba7d
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-01-29 19:00:05 +0530

    MOSIP-39239: Made the success message does look clearly distinguish the name of the published policy
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- pmp-revamp-ui/src/pages/common/SuccessMessage.js
+++ pmp-revamp-ui/src/pages/common/SuccessMessage.js
@@ -2,7 +2,7 @@ import { getUserProfile } from '../../services/UserProfileService';
 import cancelIcon from '../../svg/cancel_icon.svg';
 import { isLangRTL } from '../../utils/AppUtils';
 
-function SuccessMessage({ successMsg, clickOnCancel, customStyle, publishedPolicyName }) {
+function SuccessMessage({ successMsg, clickOnCancel, customStyle, successParam }) {
 
     const isLoginLanguageRTL = isLangRTL(getUserProfile().langCode);
 
@@ -10,10 +10,10 @@ function SuccessMessage({ successMsg, clickOnCancel, customStyle, publishedPolic
         <div className={`${customStyle ? customStyle.outerDiv : `flex justify-end max-w-7xl my-3 absolute ${isLoginLanguageRTL ? "left-0.5" : "right-0.5"}`}`}>
             <div className={` bg-fruit-salad ${customStyle ? customStyle.innerDiv : 'flex justify-between items-center rounded-xl max-w-[35rem] min-h-14 min-w-72 p-4'}`}>
                 <div className={`${isLoginLanguageRTL ? 'ml-6' : 'mr-6'} w-[90%]`}>
-                    {!publishedPolicyName 
+                    {!successParam 
                     ? <p className="text-sm/4 text-white break-words font-inter" dangerouslySetInnerHTML={{ __html: successMsg }} /> 
                     : <p className="text-sm/4 text-white break-words font-normal">
-                        <span className='font-extrabold'>{publishedPolicyName}</span>{successMsg}
+                        <span className='font-bold'>{successParam}</span>{successMsg}
                     </p>
                     }
                     
