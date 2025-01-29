39556bc1d27fd3c86ffccd37266255d30aad4360
anup-nehe <anup.nehe@technoforte.co.in>
2025-01-29 15:58:34 +0530

    Update entrypoint.sh
    Signed-off-by: anup-nehe <anup.nehe@technoforte.co.in>

--- uitest-pmprevamp/entrypoint.sh
+++ uitest-pmprevamp/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 java --version
-<<<<<<< HEAD
-<<<<<<< HEAD
-java -jar pmpui-*-jar-with-dependencies.jar
-=======
 java -jar pmprevampui-*-jar-with-dependencies.jar
->>>>>>> upstream/develop
-=======
-java -jar pmprevampui-*-jar-with-dependencies.jar
->>>>>>> 4c1a11646637f589166dafab45b84a7bf3242405
