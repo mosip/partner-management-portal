16ffc76243960eac7a735e1b5312ad0d630f85dc
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2025-03-27 14:54:04 +0530

    MOSIP-39852: Updated title, descriptions, field name as per story (#1272)
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>
    Co-authored-by: Swetha K <swetha.k@technoforte.co.in>

--- pmp-revamp-ui/src/pages/partner/notifications/PartnerNotificationsTab.js
+++ pmp-revamp-ui/src/pages/partner/notifications/PartnerNotificationsTab.js
@@ -1,27 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import { isLangRTL } from "../../../utils/AppUtils";
-import { getUserProfile } from "../../../services/UserProfileService";
 
-function PartnerNotificationsTab({ partnerType, activeTab }) {
+function PartnerNotificationsTab({ activeTab }) {
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const isLoginLanguageRTL = isLangRTL(getUserProfile().langCode);
 
     const changeToPartnerCert = () => {
         navigate('/partnermanagement/notifications/view-partner-certificate-expiry');
-    };
-
-    const changeToFtmCert = () => {
-        navigate('/partnermanagement/notifications/view-ftm-chip-certificate-expiry')
-    };
-
-    const changeToApiKey = () => {
-        navigate('/partnermanagement/notifications/view-api-key-expiry')
-    };
-
-    const changeToSbi = () => {
-        navigate('/partnermanagement/notifications/view-sbi-expiry')
     };
 
     return (
@@ -33,30 +18,6 @@ function PartnerNotificationsTab({ partnerType, activeTab }) {
 
                 <div className={`h-1 w-full ${activeTab === "partner" ? "bg-tory-blue" : "bg-transparent"}  rounded-t-md`}></div>
             </div>
-            {partnerType === "FTM_PROVIDER" && (
-                <div id='ftm_cert_notifications_tab' className={`flex-col justify-center text-center`}>
-                    <button onClick={changeToFtmCert} className={`${activeTab === "ftm" ? "text-[#1447b2]" : "text-[#031640]"} py-4 cursor-pointer text-base ${isLoginLanguageRTL && 'mr-10'}`}>
-                        <h6> {t('partnerNotificationsTab.ftmCertificate')}</h6>
-                    </button>
-                    <div className={`h-1 w-full ${isLoginLanguageRTL && 'mr-6'} ${activeTab === "ftm" ? "bg-tory-blue" : "bg-transparent"} rounded-t-md`}></div>
-                </div>
-            )}
-            {partnerType === "AUTH_PARTNER" && (
-            <div id='api_key_notifications_tab' className={`flex-col justify-center text-center`}>
-                <button onClick={changeToApiKey} className={`${activeTab === "apikey" ? "text-[#1447b2]" : "text-[#031640]"} py-4 cursor-pointer text-base ${isLoginLanguageRTL && 'mr-10'}`}>
-                    <h6> {t('partnerNotificationsTab.apikey')}</h6>
-                </button>
-                <div className={`h-1 w-full ${isLoginLanguageRTL && 'mr-6'} ${activeTab === "apikey" ? "bg-tory-blue" : "bg-transparent"} rounded-t-md`}></div>
-            </div>
-            )}
-            {partnerType === "DEVICE_PROVIDER" && (
-            <div id='sbi_notifications_tab' className={`flex-col justify-center text-center`}>
-                <button onClick={changeToSbi} className={`${activeTab === "sbi" ? "text-[#1447b2]" : "text-[#031640]"} py-4 cursor-pointer text-base ${isLoginLanguageRTL && 'mr-10'}`}>
-                    <h6> {t('partnerNotificationsTab.sbi')}</h6>
-                </button>
-                <div className={`h-1 w-full ${isLoginLanguageRTL && 'mr-6'} ${activeTab === "sbi" ? "bg-tory-blue" : "bg-transparent"} rounded-t-md`}></div>
-            </div>
-            )}
         </div>
     );
 }
