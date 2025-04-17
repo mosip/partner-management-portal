d1805a20a8ee976f83de187f679e1efb9cad2b11
Swetha K <swetha.k@technoforte.co.in>
2025-04-17 12:58:49 +0530

    MOSIP-40869: Fixed notification icon open collapse issue
    Signed-off-by: Swetha K <swetha.k@technoforte.co.in>

--- pmp-revamp-ui/src/notificationsSlice.js
+++ pmp-revamp-ui/src/notificationsSlice.js
@@ -4,15 +4,27 @@ import { createSlice } from "@reduxjs/toolkit";
 const notificationsSlice = createSlice({
     name: "headerNotifications",
     initialState: {
-        headerNotifications: []
+        headerNotifications: [],
+        lastSeenDtimes: null,
+        notificationSeenDtimes: null,
+        dismissClicked: false,
     },
     reducers: {
         updateHeaderNotifications: (state, action) => {
             state.headerNotifications = action.payload;
         },
+        updateLastSeenDtimes: (state, action) => {
+            state.lastSeenDtimes = action.payload;
+        },
+        updateNotificationSeenDtimes: (state, action) => {
+            state.notificationSeenDtimes = action.payload;
+        },
+        updateDismissClicked: (state, action) => {
+            state.dismissClicked = action.payload;
+        },
     },
 });
 
-export const {updateHeaderNotifications} = notificationsSlice.actions;
+export const {updateHeaderNotifications, updateLastSeenDtimes, updateNotificationSeenDtimes, updateDismissClicked } = notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
