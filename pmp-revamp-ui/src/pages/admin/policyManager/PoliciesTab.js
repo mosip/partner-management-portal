930d29ad922b70831b65eebd4c7c25a7f1a19ec2
sudeep <sudeep.j7353@gmail.com>
2025-05-15 16:17:04 +0530

    MOSIP-41291 :NPM Build log warning fix
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-revamp-ui/src/pages/admin/policyManager/PoliciesTab.js
+++ pmp-revamp-ui/src/pages/admin/policyManager/PoliciesTab.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import { getUserProfile } from '../../../services/UserProfileService';
-import { isLangRTL, onPressEnterKey } from '../../../utils/AppUtils';
+import { isLangRTL } from '../../../utils/AppUtils';
 
 function PoliciesTab() {
     const { t } = useTranslation();
