3f661885c1feffe5a65c14b9a3189b60b5554a20
sudeep <sudeep.j7353@gmail.com>
2025-05-30 18:02:13 +0530

    MOSIP-41630 : For a non-valid PMS partner, when attributes are missing in Keycloak case handling
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pmp-ui-v2/src/pages/dashboard/Dashboard.js
+++ pmp-ui-v2/src/pages/dashboard/Dashboard.js
@@ -110,7 +110,7 @@ function Dashboard() {
           setIsEmailVerified(resData.emailExists);
           if (!resData.emailExists) {
             // 2. If email does not exist, check if any required attributes are missing
-            const requiredFields = ['userName', 'orgName', 'address', 'phoneNumber', 'email', 'partnerType', 'langCode'];
+            const requiredFields = ['userName', 'orgName', 'address', 'phoneNumber', 'email', 'partnerType'];
 
             const isAttributeMissing = requiredFields.some(field => {
               const value = userProfile[field];
