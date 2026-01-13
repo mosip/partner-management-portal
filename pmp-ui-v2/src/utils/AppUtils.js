21349ab92636a02edd5b5beb18e9c418df2fa780
Mayura Deshmukh <52281514+mayuradesh@users.noreply.github.com>
2026-01-13 12:57:14 +0530

    Revert "[MOSIP-44162] [MOSIP-44192] : Added proper username validation and up…"
    This reverts commit ece518119af9a3e0232e9028b3762fa114af66a4.

--- pmp-ui-v2/src/utils/AppUtils.js
+++ pmp-ui-v2/src/utils/AppUtils.js
@@ -1462,7 +1462,7 @@ export const validateUsernameRegex = (input, setInputError, t) => {
     }
     
     // Check if the rest of the username contains only valid characters
-    const validPattern = /^[\p{L}][\p{L}\p{N}\p{M}@#&()\\\-'?!":;=_]*$/u;
+    const validPattern = /^[\p{L}][\p{L}\p{N}\p{M}._-]*$/u;
     
     if (validPattern.test(input)) {
         setInputError("");
