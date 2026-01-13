a8f0924186a410284c414fee12e387e87bd1f641
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-01-13 19:33:01 +0530

    [MOSIP-44162]: Changed error code for invalid username
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/utils/AppUtils.js
+++ pmp-ui-v2/src/utils/AppUtils.js
@@ -1467,7 +1467,7 @@ export const validateUsernameRegex = (input, setInputError, t) => {
     if (validPattern.test(input)) {
         setInputError("");
     } else {
-        setInputError(t('commons.inputError'));
+        setInputError(t('commons.usernameError'));
     }
 }
 
