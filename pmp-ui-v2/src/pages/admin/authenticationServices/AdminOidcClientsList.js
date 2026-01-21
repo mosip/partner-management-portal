cbf40e33fa6b52641e541a9fca6163d384e8e046
Sudeep7353 <103890430+Sudeep7353@users.noreply.github.com>
2026-01-21 18:54:16 +0530

    MOSIP-44028: UI bug fix. (#1624)
    Signed-off-by: sudeep j <sudeepj@sudeeps-MacBook-Pro.local>
    Co-authored-by: sudeep j <sudeepj@sudeeps-MacBook-Pro.local>

--- pmp-ui-v2/src/pages/admin/authenticationServices/AdminOidcClientsList.js
+++ pmp-ui-v2/src/pages/admin/authenticationServices/AdminOidcClientsList.js
@@ -307,7 +307,7 @@ function AdminOidcClientsList() {
                                             : (
                                                 <>
                                                     <div className="mx-[1.5rem] overflow-x-scroll">
-                                                        <table className="table-fixed">
+                                                        <table className="table-fixed w-full">
                                                             <thead>
                                                                 <tr>
                                                                     {tableHeaders.map((header, index) => {
