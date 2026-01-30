4ae14c7c5aa88f2ead1c3b7fc8b3df33a6844937
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-01-30 18:07:34 +0530

    [MOSIP-44315]: OIDC client edit screen submit button and info tooltips issues fixed
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/pages/common/fields/Information.js
+++ pmp-ui-v2/src/pages/common/fields/Information.js
@@ -10,15 +10,42 @@ function Information({ infoKey, infoKey1, id }) {
     const { t } = useTranslation();
     const isLoginLanguageRTL = isLangRTL(getUserProfile().locale);
     const [showTooltip, setShowTooltip] = useState(false);
+    const [tooltipPosition, setTooltipPosition] = useState('center');
     const tooltipRef = useRef(null);
+    const iconRef = useRef(null);
 
     useEffect(() => {
         handleMouseClickForDropdown(tooltipRef, () => setShowTooltip(false));
     }, [tooltipRef])
+
+    useEffect(() => {
+        if (showTooltip && iconRef.current) {
+            const iconRect = iconRef.current.getBoundingClientRect();
+            const viewportWidth = window.innerWidth;
+            const tooltipWidth = 200;
+
+            const spaceOnLeft = iconRect.left;
+            const spaceOnRight = viewportWidth - iconRect.right;
+
+            if (spaceOnLeft > 140 && spaceOnRight > 140) {
+                setTooltipPosition('center');
+            } else if (spaceOnRight > 260) {
+                setTooltipPosition('left');
+            } else if (spaceOnLeft > 260) {
+                setTooltipPosition('right');
+            } else {
+                setTooltipPosition('center');
+            }
+        }
+    }, [showTooltip]);
+
+
+
     return (
         <div className="relative inline-flex items-center">
             <div ref={tooltipRef} className="flex items-center">
                 <div
+                    ref={iconRef}
                     id={id}
                     onClick={() => setShowTooltip(!showTooltip)}
                     className="cursor-pointer h-[13px] w-[13px] ml-1 flex-shrink-0"
@@ -31,16 +58,17 @@ function Information({ infoKey, infoKey1, id }) {
                 {showTooltip && (
                     <div
                         className={`
-                        absolute z-20 p-4 font-normal
-                        w-[20vw] max-w-[300px] max-h-[20vh] overflow-y-auto
-                        shadow-lg bg-white border border-gray-300 rounded
-                        ${isLoginLanguageRTL ? 'right-6 max-800:-right-[50%]' : 'max-800:left-1/2  left-6'} mt-2
-                        max-800:-translate-x-1/2 max-800:top-full max-800:mt-3
+                        absolute z-50 p-4 font-normal
+                        w-[min(200px,calc(100vw-4rem))] max-h-[30vh] overflow-y-auto overflow-x-hidden
+                        shadow-lg bg-white border border-gray-400 rounded
+                        ${tooltipPosition === 'left' ? 'left-0' : tooltipPosition === 'right' ? 'right-0' : 'left-1/2 -translate-x-1/2'} top-full mt-2
+                        break-words
+                        ${isLoginLanguageRTL ? 'text-right' : 'text-left'}
                         `}
                     >
                         <p id={id + '_info_description'} className="text-black text-sm">{t(infoKey)}</p>
                         {infoKey1 && (
-                        <p id={id + '_info_description1'} className="text-black text-sm">{t(infoKey1)}</p>
+                            <p id={id + '_info_description1'} className="text-black text-sm">{t(infoKey1)}</p>
                         )}
                     </div>
                 )}
