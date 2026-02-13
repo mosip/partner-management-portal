1575b3c2c22eca52b377b5d06079c3043ebc0fd2
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-02-13 15:01:17 +0530

    [MOSIP-44412]: New card for Manual adjudication (#1650)
    * [MOSIP-44412]: New card for Manual adjudication
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * [MOSIP-44412]: New card for Manual adjudication
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * [MOSIP-44412]: New card for Manual adjudication
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * [MOSIP-44412]: New card for Manual adjudication
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * Updated Manual adjudication page
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * Maintaining uique id for clean code
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * Maintaining uique id for clean code
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    ---------
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

---
+++ pmp-ui-v2/src/pages/admin/manualAdjudication/ManualAdjudicationServices.js
@@ -0,0 +1,48 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { getUserProfile } from '../../../services/UserProfileService';
+import { isLangRTL} from '../../../utils/AppUtils';
+import Title from '../../common/Title.js';
+import EmptyList from '../../common/EmptyList.js';
+
+function ManualAdjudicationServices() {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+    const isLoginLanguageRTL = isLangRTL(getUserProfile().locale);
+
+    const tableHeaders = [
+        { id: "partnerId", headerNameKey: 'manualAdjudicationServices.partnerId' },
+        { id: "orgName", headerNameKey: "manualAdjudicationServices.orgName" },
+        { id: "policyGroupName", headerNameKey: "manualAdjudicationServices.policyGroup" },
+        { id: "policyName", headerNameKey: "manualAdjudicationServices.policyName" },
+        { id: "apiKeyLabel", headerNameKey: "manualAdjudicationServices.apiKeyName" },
+        { id: "createdDateTime", headerNameKey: "manualAdjudicationServices.creationDate" },
+        { id: "status", headerNameKey: "manualAdjudicationServices.status" },
+        { id: "action", headerNameKey: 'manualAdjudicationServices.action' }
+    ];
+
+    const generateApiKey = () => {
+        // TODO: Navigate to generate API key page
+    };
+
+    return (
+        <div className={`mt-2 w-[100%] ${isLoginLanguageRTL ? "mr-28 ml-5" : "ml-28 mr-5"} overflow-x-scroll font-inter`}>
+            <div className="flex-col mt-5">
+                <div className="flex justify-between mb-5">
+                    <Title title='dashboard.manualAdjudication' backLink='/partnermanagement' />
+                </div>
+                <div className="bg-[#FCFCFC] w-full mt-3 rounded-lg shadow-lg items-center">
+                    <EmptyList
+                        tableHeaders={tableHeaders}
+                        showCustomButton={true}
+                        customButtonName='manualAdjudicationServices.generateApiKey'
+                        buttonId='generate_api_key'
+                        onClickButton={generateApiKey}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ManualAdjudicationServices;
