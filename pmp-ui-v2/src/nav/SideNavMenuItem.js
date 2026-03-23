f52648d93ad1d712ed6118848d5fb3aa703f918c
Rakshithasai123 <rakshithasai2002@gmail.com>
2026-03-23 15:11:13 +0530

    MOSIP-44656 Biometric Provider Configuration Dashboard Card in PMS
    Signed-off-by: Rakshithasai123 <rakshithasai2002@gmail.com>

--- pmp-ui-v2/src/nav/SideNavMenuItem.js
+++ pmp-ui-v2/src/nav/SideNavMenuItem.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 export const SideNavMenuItem = ({ title, id, isExpanded, activeIcon }) => {
 
     const isLoginLanguageRTL = isLangRTL(getUserProfile().locale);
-    const isActive = activeIcon === id;
     return (
         <div className="flex items-center gap-x-2 mt-2 font-inter">
             <div className={`h-6 pl-1 w-1 ${activeIcon === id ? 'bg-tory-blue' : null} rounded-e-md`}></div>
