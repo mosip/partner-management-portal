72478291051d2cb77737ea2accaa9f859e0c5b5c
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-03-24 13:20:39 +0530

    [MOSIP-44595] Create new bio extractor configuration (#1685)
    * [MOSIP-44595]: Form for creating new bio extractor configuration
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * [MOSIP-44595]: Form for creating new bio extractor configuration
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * [MOSIP-44595]: Form for creating new bio extractor configuration
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    ---------
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/pages/admin/biometricProviderConfigurationList/BiometricProviderConfigurationList.js
+++ pmp-ui-v2/src/pages/admin/biometricProviderConfigurationList/BiometricProviderConfigurationList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Title from "../../common/Title";
 import { useTranslation } from "react-i18next";
-import { isLangRTL } from "../../../utils/AppUtils";
+import { isLangRTL, onPressEnterKey } from "../../../utils/AppUtils";
 import { getUserProfile } from "../../../services/UserProfileService";
 
 function BiometricProviderConfigurationList() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const isLoginLanguageRTL = isLangRTL(getUserProfile().locale);
 
   return (
@@ -16,6 +18,15 @@ function BiometricProviderConfigurationList() {
           subTitle="dashboard.biometricProviderConfigurationDesc"
           backLink="/partnermanagement"
         />
+        <button
+          id="bio_extractor_config_create_btn"
+          type="button"
+          onClick={() => navigate('/partnermanagement/admin/biometric-provider-configuration/create')}
+          onKeyDown={(e) => onPressEnterKey(e, () => navigate('/partnermanagement/admin/biometric-provider-configuration/create'))}
+          className="h-10 text-sm font-semibold px-4 py-2 bg-tory-blue text-white border border-[#1447B2] rounded-md"
+        >
+          {t('bioExtractorConfig.createBioExtractorConfig')}
+        </button>
       </div>
     </div>
   );
