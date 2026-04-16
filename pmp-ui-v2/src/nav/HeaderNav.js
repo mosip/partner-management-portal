e73422844d5b280c5548b890e2819a0bd111be77
Rakshithasai123 <rakshithasai2002@gmail.com>
2026-04-16 15:26:40 +0530

    [MOSIP-44648] fixed the header space
    Signed-off-by: Rakshithasai123 <rakshithasai2002@gmail.com>
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/nav/HeaderNav.js
+++ pmp-ui-v2/src/nav/HeaderNav.js
@@ -206,8 +206,7 @@ function HeaderNav({ open, setOpen }) {
     }
 
     return (
-        <nav className="sticky top-0 z-50 bg-white flex justify-between w-full h-14 font-inter 
-            shadow-[rgba(0,0,0,0.13)_5px_2px_8px_0px] ">
+        <nav className="sticky top-0 left-0 right-0 z-50 bg-white flex justify-between h-14 w-full">
             <div className={`flex gap-x-4 h-14 items-center shadow-sm`}>
                 {!open && (
                     <div className={`flex items-center ${isLoginLanguageRTL ? "pr-5" : "pl-6"}`}>
