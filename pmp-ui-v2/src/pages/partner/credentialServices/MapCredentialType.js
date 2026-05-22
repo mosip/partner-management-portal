a85dea2c6a71a44b97cbcf002509f3040d53a815
Rakshithasai123 <rakshithasai2002@gmail.com>
2026-05-22 17:20:40 +0530

    file fix
    Signed-off-by: Rakshithasai123 <rakshithasai2002@gmail.com>

--- pmp-ui-v2/src/pages/partner/credentialServices/MapCredentialType.js
+++ pmp-ui-v2/src/pages/partner/credentialServices/MapCredentialType.js
@@ -268,7 +268,7 @@ function MapCredentialType() {
         const timestamp = new Date().toISOString();
     request.requestTime = timestamp;
       const url = getPartnerManagerUrl(
-        `/partner-policy-requests/${requestIdForBioApi}/credential-types-request`,
+        `/partners/partner-policy-requests/${requestIdForBioApi}/credential-types-request`,
         process.env.NODE_ENV
       );
 
