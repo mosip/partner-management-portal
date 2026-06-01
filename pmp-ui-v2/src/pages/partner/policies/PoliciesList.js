fb66718d8e06cb361d5e82c3aa867f14edbf87ea
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-06-01 15:08:04 +0530

    updated
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/pages/partner/policies/PoliciesList.js
+++ pmp-ui-v2/src/pages/partner/policies/PoliciesList.js
@@ -551,7 +551,6 @@ function PoliciesList() {
                                             })()}
                                           </>
                                         )}
-
                                       </div>
                                       );
                                     })()}
