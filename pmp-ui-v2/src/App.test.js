810812110282f16f9f7463849873b511442e7f87
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2026-06-23 13:27:36 +0530

    #1753 Moved top-level await out of App.js to fix Jest test failure (#1783)
    * #1777 NEW package-lock.json file
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * Add CI test script to package.json
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * move top-level await out of App.js to fix Jest test failure
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    * Fixed
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
    
    ---------
    
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- pmp-ui-v2/src/App.test.js
+++ pmp-ui-v2/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('./AppRoutes', () => () => <div data-testid="app-routes" />);
+jest.mock('./services/ConfigService.js', () => ({ loadAppConfig: jest.fn(() => Promise.resolve()) }));
+
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
